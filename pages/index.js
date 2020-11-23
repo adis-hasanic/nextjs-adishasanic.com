620d8dd5e8c0fb4965685be15c3ee4cf000b53d0
adis-hasanic <adisdesigner96@gmail.com>
2020-11-24 00:24:54 +0100

    pages

--- pages/index.js
+++ pages/index.js
@@ -1,65 +1,75 @@
 import Head from 'next/head'
-import styles from '../styles/Home.module.css'
+import { categories } from '../data/product.js'
+import Link from 'next/link'
+
 
 export default function Home() {
-  return (
-    <div className={styles.container}>
-      <Head>
-        <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
+    return (
+        <div>
+            <Head>
+                <title>Adis Hasanic</title>
+                <link rel="icon" href="/favicon.ico" />
+            </Head>
 
-      <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
+            <a className="contact-button" href="https://calendly.com/adishasanic/60min?month=2020-11">Contact</a>
 
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
-        </p>
+            <div className="content">
+                
 
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
+                <div className="section portfolio">
 
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
+                <div className="col">
+                    <div className="category">
+                        <div className="intro">Digital Product Design (UI/UX)</div>
+                        {categories[0].projects.map(project => {
+                            return (
+                                <Link key={project.id} href={`/portfolio/${categories[0].id}?project=${project.id}`} >
+                                    <a href={`/portfolio/${categories[0].id}?project=${project.id}`} >
+                                    <div className="work-thumbnail">
+                                        <span className="title">{project.id}</span>
+                                        <span className="description">{project.description}</span>
+                                    </div>
+                                    </a>
+                                </Link>
+                            )
+                        })}
+                    </div>
+                </div>
 
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className={styles.card}
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
+                <div className="col">
+                    <div className="category">
+                        <div className="intro">Web Design</div>
+                        {categories[1].projects.map(project => {
+                            return (
+                                <Link key={project.id} href={`/portfolio/${categories[1].id}?project=${project.id}`} >
+                                    <div className="work-thumbnail">
+                                        <span className="title">{project.id}</span>
+                                        <span className="description">{project.description}</span>
+                                    </div>
+                                </Link>
+                            )
+                        })}
+                    </div>
+                    <div className="category">
+                        <div className="intro">Visual</div>
+                        {categories[2].projects.map(project => {
+                            return (
+                                <Link key={project.id} href={`/portfolio/${categories[2].id}?project=${project.id}`} >
+                                    <div className="work-thumbnail">
+                                        <span className="title">{project.id}</span>
+                                        <span className="description">{project.description}</span>
+                                    </div>
+                                </Link>
+                            )
+                        })}
+                    </div>
+                    
+                </div>
 
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
+                </div>
+
+            </div>
+
         </div>
-      </main>
-
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
-        </a>
-      </footer>
-    </div>
-  )
+    )
 }
---
+++ pages/portfolio/[id].js
@@ -0,0 +1,54 @@
+import Head from 'next/head'
+import Link from 'next/link'
+import {useRouter} from 'next/router'
+import {categories} from '../../data/product.js'
+
+
+
+const Story = () => {
+    const router = useRouter()
+    const {id, project} = router.query
+    let category = categories.find(o => o.id == id)
+    if (category) {
+        let story = category.projects.find(o => o.id == project)
+
+        if (story) {
+            return(
+
+                
+            <div>
+
+                
+                 <Head>
+                    <title>{story.id}</title>
+                </Head>
+                
+                <div className="top-bar"><Link href="/" ><div className="back">← Back</div></Link></div>
+                <a className="contact-button" href="https://calendly.com/adishasanic/60min?month=2020-11">Contact</a>
+
+                <div className="story-page">
+                {story.paragraphs.map(paragraph => {
+                    return(
+                    <div key={paragraph.id} className={paragraph.imageClass}>
+                    <img className={paragraph.imageClass ? paragraph.imageClass : ""} src={paragraph.imageURL} ></img>
+                    </div>
+                    )
+                })}
+                </div>
+            </div>
+            )
+        }
+        else {
+            return(
+            <div>loading</div>
+            )
+        }
+    }
+    else {
+        return(
+        <div>loading</div>
+        )
+    }
+}
+
+export default Story;
