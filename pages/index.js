d1f0f03cf3499d45c74cff7e1557b97656792ec0
adis-hasanic <adisdesigner96@gmail.com>
2020-11-24 13:48:37 +0100

    willing

--- pages/index.js
+++ pages/index.js
@@ -12,13 +12,17 @@ export default function Home() {
                 <link rel="icon" href="/favicon.svg" />
 
 <script async src="https://www.googletagmanager.com/gtag/js?id=UA-121629290-2"></script>
-<script>
+<script
+    dangerouslySetInnerHTML={{
+      __html: `
   window.dataLayer = window.dataLayer || [];
   function gtag(){dataLayer.push(arguments);}
   gtag('js', new Date());
 
   gtag('config', 'UA-121629290-2');
-</script>
+  `,
+}}
+/>
 
             </Head>
 
