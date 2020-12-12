a5316973f32225e3918f78bf890b35072bedd582
adis-hasanic <adisdesigner96@gmail.com>
2020-12-12 01:40:56 +0100

    willing

--- pages/flowdash.js
+++ pages/flowdash.js
@@ -9,10 +9,13 @@ export default function Home() {
           <h1>Flowdash</h1>
         </div>
         <div className="row">
-          <p>In August 2020, hired by Flowdash, a YC-backed San Francisco based startup, to work the design of their product. Working closely with CEO and CTO on redesigning Flowdash's product and on designing new features.</p>
+          <p>Since August 2020, hired by Flowdash, a YC-backed San Francisco based startup, to work on the design of their product.</p>
         </div>
         <div className="row">
-          <p>The problem was mainly that people who sign up do not learn how to build a workflow on their own. After about 3 months, the new version was launched on ProductHunt: got more than 2000 upvotes, positive feedback on product and design, and everyone who tried it went through the onboarding successfully.</p>
+          <p>Recently, after 3 months of working closely with founders of Flowdash (Nick and Omar), they launched the new version on ProductHunt: #2 product of the month, almost 3000 upvotes, positive feedback on product and design, and everyone who tried it went through the onboarding successfully.</p>
+        </div>
+        <div className="row">
+          <p><iframe className="producthunt" src="https://cards.producthunt.com/cards/posts/275215?v=1" height="512" frameborder="0" scrolling="no" allowfullscreen></iframe></p>
         </div>
         <div className="row">
           <img src="/portfolio/product/flowdash/1.jpg" className="eleven-twenty border"></img>
