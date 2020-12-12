e236c283c1217f578b237f1ee21230b769874dfa
adis-hasanic <adisdesigner96@gmail.com>
2020-12-12 13:30:09 +0100

    willing

--- pages/portal.js
+++ pages/portal.js
@@ -24,7 +24,16 @@ export default function Home() {
           <img src="/portfolio/product/portal/1/4.jpg" className="twelve-eighty"></img>
         </div>
         <div className="row">
+          <img src="/portfolio/product/portal/1/5.jpg" className="twelve-eighty"></img>
+        </div>
+        <div className="row">
           <img src="/portfolio/product/portal/2/1.jpg" className="twelve-eighty"></img>
+        </div>
+        <div className="row">
+          <img src="/portfolio/product/portal/2/1-2.jpg" className="twelve-eighty"></img>
+        </div>
+        <div className="row">
+          <img src="/portfolio/product/portal/2/1-3.jpg" className="twelve-eighty"></img>
         </div>
         <div className="row">
           <img src="/portfolio/product/portal/2/2.jpg" className="twelve-eighty"></img>
@@ -33,7 +42,31 @@ export default function Home() {
           <img src="/portfolio/product/portal/2/3.jpg" className="twelve-eighty"></img>
         </div>
         <div className="row">
+          <img src="/portfolio/product/portal/2/3-1.jpg" className="twelve-eighty"></img>
+        </div>
+        <div className="row">
           <img src="/portfolio/product/portal/2/4.jpg" className="twelve-eighty"></img>
+        </div>
+        <div className="row">
+          <img src="/portfolio/product/portal/2/5.jpg" className="twelve-eighty"></img>
+        </div>
+        <div className="row">
+          <img src="/portfolio/product/portal/2/6.jpg" className="twelve-eighty"></img>
+        </div>
+        <div className="row">
+          <img src="/portfolio/product/portal/2/7.jpg" className="twelve-eighty"></img>
+        </div>
+        <div className="row">
+          <img src="/portfolio/product/portal/2/8.jpg" className="twelve-eighty"></img>
+        </div>
+        <div className="row">
+          <img src="/portfolio/product/portal/2/9.jpg" className="twelve-eighty"></img>
+        </div>
+        <div className="row">
+          <img src="/portfolio/product/portal/2/10.jpg" className="twelve-eighty"></img>
+        </div>
+        <div className="row">
+          <img src="/portfolio/product/portal/3/0.jpg" className="twelve-eighty"></img>
         </div>
         <div className="row">
           <img src="/portfolio/product/portal/3/1.jpg" className="twelve-eighty"></img>
@@ -42,10 +75,25 @@ export default function Home() {
           <img src="/portfolio/product/portal/3/2.jpg" className="twelve-eighty"></img>
         </div>
         <div className="row">
+          <img src="/portfolio/product/portal/3/2-1.jpg" className="twelve-eighty"></img>
+        </div>
+        <div className="row">
           <img src="/portfolio/product/portal/3/3.jpg" className="twelve-eighty"></img>
         </div>
         <div className="row">
+          <img src="/portfolio/product/portal/3/4.jpg" className="twelve-eighty"></img>
+        </div>
+        <div className="row">
           <img src="/portfolio/product/portal/4/1.jpg" className="twelve-eighty"></img>
+        </div>
+        <div className="row">
+          <img src="/portfolio/product/portal/4/1-2.jpg" className="twelve-eighty"></img>
+        </div>
+        <div className="row">
+          <img src="/portfolio/product/portal/4/1-3.jpg" className="twelve-eighty"></img>
+        </div>
+        <div className="row">
+          <img src="/portfolio/product/portal/4/2.jpg" className="twelve-eighty"></img>
         </div>
       </div>
     </div>
