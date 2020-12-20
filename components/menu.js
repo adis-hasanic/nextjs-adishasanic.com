a79721b98a20483202cd0818738724b870892d77
adis-hasanic <adisdesigner96@gmail.com>
2020-12-20 20:55:38 +0100

    willing

--- components/menu.js
+++ components/menu.js
@@ -35,18 +35,18 @@ const Menu = () => {
             <div className="links">
                 <Link href="/flowdash"><div className={isActive('/flowdash')} ><span className="name">Flowdash</span><span className="description">Build apps for team workflows without coding.</span></div></Link>
                 <Link href="/stacker"><div className={isActive('/stacker')}><span className="name">Stacker</span><span className="description">Turn spreadsheets into customizable apps.</span></div></Link>
-                <Link href="/teamscope"><div className={isActive('/teamscope')}><span className="name">Teamscope</span><span className="description">Analyze people (personality, values, etc.) to build teams.</span></div></Link>
+                <Link href="/teamscope"><div className={isActive('/teamscope')}><span className="name">Teamscope</span><span className="description">Analyze personality and values to build teams.</span></div></Link>
                 <Link href="/amigos"><div className={isActive('/amigos')}><span className="name">Amigos</span><span className="description">Car-search and marketplace for iOS.</span></div></Link>
-                <Link href="/teleport"><div className={isActive('/teleport')}><span className="name">Teleport</span><span className="description"><i>Uber</i> for deliveries. Winner of a 3-day hackathon in Estonia.</span></div></Link>
-                <Link href="/portal"><div className={isActive('/portal')}><span className="name">Portal</span><span className="description">Cryptocurrency wallet, transfer, exchange, and conversion.</span></div></Link>
-                <Link href="/indiecampers"><div className={isActive('/indiecampers')}><span className="name">IndieCampers</span><span className="description">Book a campervan for a trip using a mobile website.</span></div></Link>
+                <Link href="/teleport"><div className={isActive('/teleport')}><span className="name">Teleport by ZITICITY</span><span className="description"><i>Uber</i> for deliveries (Garage48 hackathon winner)</span></div></Link>
+                <Link href="/portal"><div className={isActive('/portal')}><span className="name">Portal</span><span className="description">Cryptocurrency wallet and exchange.</span></div></Link>
+                <Link href="/indiecampers"><div className={isActive('/indiecampers')}><span className="name">IndieCampers</span><span className="description">Book a campervan using a mobile website.</span></div></Link>
                 <Link href="/engine"><div className={isActive('/engine')}><span className="name">Engine</span><span className="description">Build and manage a custom online shop.</span></div></Link>
-                <Link href="/liftigniter"><div className={isActive('/liftigniter')}><span className="name">LiftIgniter</span><span className="description">Connect and manage an API for content recommendation.</span></div></Link>
-                <Link href="/typen"><div className={isActive('/typen')}><span className="name">Typen</span><span className="description">Distraction-free writing app with a customizable look.</span></div></Link>
-                <Link href="/helpjuice"><div className={isActive('/helpjuice')}><span className="name">Helpjuice</span><span className="description">Build a knowledge-base (internal, help center, etc.)</span></div></Link>
+                <Link href="/liftigniter"><div className={isActive('/liftigniter')}><span className="name">LiftIgniter</span><span className="description">API for content recommendations.</span></div></Link>
+                <Link href="/typen"><div className={isActive('/typen')}><span className="name">Typen</span><span className="description">Distraction-free writing app.</span></div></Link>
+                <Link href="/helpjuice"><div className={isActive('/helpjuice')}><span className="name">Helpjuice</span><span className="description">Build and manage a knowledge-base.</span></div></Link>
                 <Link href="/foodmunk"><div className={isActive('/foodmunk')}><span className="name">Foodmunk</span><span className="description">Find a perfect meal nearby, stay healthy and fit.</span></div></Link>
                 <Link href="/kamcord"><div className={isActive('/kamcord')}><span className="name">Kamcord</span><span className="description">Mobile games gameplays and livestreams.</span></div></Link>
-                <Link href="/brandedme"><div className={isActive('/brandedme')}><span className="name">Branded.me</span><span className="description">Mobile games gameplays and livestreams.</span></div></Link>
+                <Link href="/brandedme"><div className={isActive('/brandedme')}><span className="name">Branded.me</span><span className="description">Build your personal website without coding.</span></div></Link>
             </div>
             <div className="links">
             <a href="cv.pdf"><div>Download CV</div></a>
