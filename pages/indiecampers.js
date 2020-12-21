b0980ae1f57175e1c92e9ec8fca17afcaea5f7c5
adis-hasanic <adisdesigner96@gmail.com>
2020-12-21 13:40:52 +0100

    willing

--- pages/indiecampers.js
+++ pages/indiecampers.js
@@ -10,7 +10,7 @@ export default function Home() {
           <h2>Book a campervan using a mobile website.</h2>
         </div>
         <div className="row">
-          <p>In October 2018, as a part of a job application process to IndieCampers — a Lisbon based Europe's biggest campervan rental service — I was asked by IndieCampers to do a quick 1-week redesign of experience of booking a campervan via a mobile website. It led to an offer for a full-time role.</p>
+          <p>In October 2018, as a part of a job application process to IndieCampers, Europe's biggest campervan rental service based in Lisbon, Portugal, I was asked by IndieCampers to do a quick 1-week redesign of experience of booking a campervan via a mobile website. I turned 5 pages into 1, set a new visual direction, and simplified process in lots of small ways. IndieCampers CEO and PM were impressed and offered me a full-time role.</p>
         </div>
         <div className="row">
           <img src="/portfolio/quick/indiecampers/1.jpg" className="three-twenty border"></img>
