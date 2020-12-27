6df2cdf1c06ade9c4471acdc128634ee51f44137
adis-hasanic <adisdesigner96@gmail.com>
2020-12-27 21:54:48 +0100

    test

--- pages/teamscope.js
+++ pages/teamscope.js
@@ -10,21 +10,17 @@ export default function Home() {
           <h2>Analyze personality and values to build teams.</h2>
         </div>
         <div className="row">
-          <p>From Oct 2018 to Jan 2020, I was a sole designer at Teamscope in Tallinn, Estonia. Teamscope is a startup building a talent-analytics platform that uses surveys and machine learning to collect data about people's personality, values and competencies, and then presents insights that help organizations build high-performing teams. Teamscope investors include TechStars, SAP, BlackPearls and Jaan Tallinn (co-founder of Skype).</p>
-        </div>
-        <div className="row">
-          <p>Reporting to and collaborating with Founders and Product Manager, I worked on a product, focusing on making it easy to gather data from surveys, learn about people and teams from their profiles, and learn about how candidates fit into the team in the hiring process. I also designed a marketing website (conversion rate increased from 5-10 bookings per month to 25-30) and various visual material for sales and presentations.</p>
+          <p>From October 2018 to January 2020, I was a sole designer at Teamscope, an early-stage startup based in Tallinn, building a platform that helps companies analyze their people's personality, values and competencies, to build high-performing teams and improve employee onboarding process. Backers of Teamscope included TechStars, SAP, BlackPearls and Jaan Tallinn (co-founder of Skype).</p>
         </div>
         <div className="row">
           <img src="/portfolio/product/teamscope/team-photo.jpg" className="eight-hundred"></img><br></br>
           <span className="caption">Me (first from the right) with the rest of Teamscope at headquarters on January 2019 in Tallinn, Estonia.</span>
         </div>
-        <div className="row businesscards">
-          <img src="/portfolio/visual/teamscope/businesscard-front.jpg" className="five-thirty border"></img>
-          <img src="/portfolio/visual/teamscope/businesscard-back.jpg" className="five-thirty border"></img>
+        <div className="row">
+          <h3>Product</h3>
         </div>
         <div className="row">
-          <img src="/portfolio/web/teamscope-2020/home.jpg" className="forteen-forty border"></img>
+          <p>Reporting to and collaborating with Founders and Product Manager, I worked on a product, focusing on making it easy to gather data from surveys, learn about people and teams from their profiles, and learn about how candidates fit into the team in the hiring process.</p>
         </div>
         <div className="row">
           <img src="/portfolio/product/teamscope/1/1.jpg" className="twelve-eighty border"></img>
@@ -34,6 +30,9 @@ export default function Home() {
         </div>
         <div className="row">
           <img src="/portfolio/product/teamscope/1/3.jpg" className="twelve-eighty border"></img>
+        </div>
+        <div className="row">
+          <img src="/portfolio/product/teamscope/1/4.jpg" className="twelve-eighty border"></img>
         </div>
         <div className="row">
           <img src="/portfolio/product/teamscope/2/1.jpg" className="twelve-eighty border"></img>
@@ -52,6 +51,46 @@ export default function Home() {
         </div>
         <div className="row">
           <img src="/portfolio/product/teamscope/4/2.jpg" className="twelve-eighty border"></img>
+        </div>
+        <div className="row">
+          <h3>Website design in 2020</h3>
+        </div>
+        <div className="row">
+          <p>In January 2020, with the new product design with all the new features and visual direction established, the goal was to design a new version of the website that positions Teamscope as a decision-making tool to build teams in any company, not just as an analytics tool in executive search. I designed and coded the new version in 3 weeks.</p>
+        </div>
+        <div className="row">
+          <img src="/portfolio/web/teamscope-2020/home.jpg" className="forteen-forty border"></img>
+        </div>
+        <div className="row">
+          <img src="/portfolio/web/teamscope-2020/bookademo.jpg" className="forteen-forty border"></img>
+        </div>
+        <div className="row">
+          <h3>Business card</h3>
+        </div>
+        <div className="row">
+          <p>I designed a Teamscope business card in 2019. Tried to keep it clean, to match the visual style established in the app and the simplicity of learning about people using Teamscope.</p>
+        </div>
+        <div className="row businesscards">
+          <img src="/portfolio/visual/teamscope/businesscard-front.jpg" className="five-thirty border"></img>
+          <img src="/portfolio/visual/teamscope/businesscard-back.jpg" className="five-thirty border"></img>
+        </div>
+        <div className="row">
+          <h3>Website design in 2019</h3>
+        </div>
+        <div className="row">
+          <p>In mid 2019, after having designed parts of product, I did a complete redesign of the website, where the focus was to present Teamscope as a talent-analytics platform focusing on executive search. So, it had to focus on analytics capabilities and look corporate and professional.</p>
+        </div>
+        <div className="row">
+          <img src="/portfolio/web/teamscope-2019/home.jpg" className="forteen-forty border"></img>
+        </div>
+        <div className="row">
+          <img src="/portfolio/web/teamscope-2019/howitworks.jpg" className="forteen-forty border"></img>
+        </div>
+        <div className="row">
+          <img src="/portfolio/web/teamscope-2019/science.jpg" className="forteen-forty border"></img>
+        </div>
+        <div className="row">
+          <img src="/portfolio/web/teamscope-2019/bookademo.jpg" className="forteen-forty border"></img>
         </div>
       </div>
     </div>
