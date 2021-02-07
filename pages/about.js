e0034f5a19897fdd0fb19543632501b3c67e2bd9
adis-hasanic <adisdesigner96@gmail.com>
2021-02-07 13:44:28 +0100

    test

--- pages/about.js
+++ pages/about.js
@@ -10,7 +10,7 @@ export default function Home() {
             <img src="/references/adis-hasanic.jpg"></img>
             <div className="about-text">
               <p>I’m Adis, a 24-year-old creative from Bosnia. Passionate about screen, images, products, startups, storytelling, film and text. Artist at heart, but I love designing above all and creating change and value via business.</p>
-              <p>Designing at early-stage tech startups past 10 years, primarily UI and UX, sometimes websites and visuals (slides, pages, etc.). Mostly remotely, with on-site 1 year in San Francisco and 1 year in Tallinn. I solve problems via explorations and making mockups fast (using Figma), look for concerns with any solution, like to hear and discuss feedback on drafts often, and always look for ways to learn more about everything and test ideas.</p>
+              <p>Designing at early-stage tech startups past 10 years, primarily UI and UX, sometimes websites and visuals (slides, pages, etc.). Mostly remotely, with on-site 1 year in San Francisco and 1 year in Tallinn. I solve problems via explorations and making mockups fast (using Figma), look for concerns with any direction or solution, like to hear and discuss feedback on drafts often, and always look for ways to learn more about everything and test ideas.</p>
               <p><b>Looking to join an early-stage startup in Europe (visa required).</b><br></br><a>adishasanic96@gmail.com</a></p>
             </div>
           </div>
