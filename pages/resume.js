155e08a853a7189e16526d65f22d00477bcfd06a
adis-hasanic <adisdesigner96@gmail.com>
2021-02-13 16:01:14 +0100

    test

--- pages/resume.js
+++ pages/resume.js
@@ -37,7 +37,7 @@ export default function Home() {
             </div>
 
             <div className="experience">
-              <div>Was supposed join Stacker as employee and move to London, but expectations on design process from the founders at the time did not align at all with my goals and expectations then, and visa process was made impossible by Covid-19. So, I continued with job-search again and freelancing remotely.</div>
+              <div>Was supposed join Stacker as employee and move to London, but expectations on design process from the founders at the time did not align at all with my goals, preferences and expectations. Then visa process was made impossible by COVID-19. So, I continued with job-search again and freelancing remotely.</div>
             </div>
 
             <div className="experience">
