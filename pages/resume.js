01946f71e628d62b4ede90520ce46073857d639c
adis-hasanic <adisdesigner96@gmail.com>
2021-02-13 17:02:47 +0100

    test

--- pages/resume.js
+++ pages/resume.js
@@ -8,14 +8,14 @@ export default function Home() {
         <div className="row">
           <div className="about">
 
-            <div className="experience">
+            <div className="experience text">
               <div>Currently looking for a full-time role, while freelancing.</div>
             </div>
 
             <div className="experience">
               <div className="type">Freelance</div>
               <div className="spacetime">Ongoing (since Feb 2021) · Remote</div>
-              <div className="name">Stacker</div>
+              <div className="name"><a href="https://stackerhq.com" target="_blank">Stacker</a></div>
               <div>Early-stage 12-person startup based in London. Backed by YC, Initialized Capital, etc.<br></br> Create apps, without coding, using data in Airtable, Google Sheets, etc.</div>
               <div className="role">UX, UI, Web, Visual</div>
             </div>
@@ -23,7 +23,7 @@ export default function Home() {
             <div className="experience">
               <div className="type">Freelance</div>
               <div className="spacetime">1 month (Jan 2021) · Remote</div>
-              <div className="name">GigaPay</div>
+              <div className="name"><a href="https://gigapay.co" target="_blank">GigaPay</a></div>
               <div>Early-stage 10-person startup based in Stockholm. Pre-seed.<br></br> Send a payment as a fully compliant salary for gig work and similar.</div>
               <div className="role">UX/UI, Web, Visual</div>
             </div>
@@ -31,24 +31,24 @@ export default function Home() {
             <div className="experience">
               <div className="type">Freelance</div>
               <div className="spacetime">4 months (Aug 2020 – Dec 2020) · Remote</div>
-              <div className="name">Flowdash</div>
+              <div className="name"><a href="https://flowdash.com" target="_blank">Flowdash</a></div>
               <div>Early-stage 2-person startup based in San Francisco. Backed by YC.<br></br> Create apps for team workflows and business processes.</div>
               <div className="role">UX/UI</div>
             </div>
 
-            <div className="experience">
+            <div className="experience text">
               <div>Was supposed join Stacker as employee and move to London, but expectations on design process from the founders at the time did not align at all with my goals, preferences and expectations. Then visa process was made impossible by COVID-19. So, I continued with job-search again and freelancing remotely.</div>
             </div>
 
             <div className="experience">
               <div className="type">Freelance</div>
               <div className="spacetime">4 months (Feb 2020 – May 2020) · Remote</div>
-              <div className="name">Stacker</div>
+              <div className="name"><a href="https://stackerhq.com" target="_blank">Stacker</a></div>
               <div>Early-stage 6-person startup based in London. Backed by Pentech.<br></br> Create apps, without coding, using data in Airtable, Google Sheets, etc.</div>
               <div className="role">UX/UI, Web, Visual</div>
             </div>
 
-            <div className="experience">
+            <div className="experience text">
               <div>The subject of people-analytics in the context of hiring and team-building turned out not as interesting to me as I thought it'd be.</div>
             </div>
 
@@ -56,14 +56,14 @@ export default function Home() {
               <div className="type">Hackathon</div>
               <div className="spacetime">3 days (Jun 2019) · Tallinn, Estonia</div>
               <div className="name">Teleport</div>
-              <div>BaseCamp Spring 2019 was a 3-day hackathon hosted by Garage48 and Superangel VC.<br></br> I joined CEO and CTO of ZITICITY, we won with "Uber for deliveries" prototype.</div>
+              <div>BaseCamp Spring 2019 was a 3-day hackathon hosted by <a href="https://garage48.org/" target="_blank">Garage48</a> and <a href="https://www.superangel.io/" target="_blank">Superangel</a>.<br></br> I joined CEO and CTO of <a href="http://ziticity.com/" target="_blank">ZITICITY</a>, we won with "Uber for deliveries" prototype.</div>
               <div className="role">UX/UI, Web, Visual</div>
             </div>
 
             <div className="experience">
               <div className="type">Employee</div>
               <div className="spacetime">1 year (Jan 2019 – Jan 2020) · Tallinn, Estonia</div>
-              <div className="name">Teamscope</div>
+              <div className="name"><a href="https://teamscope.io/" target="_blank">Teamscope</a></div>
               <div>Early-stage 6-person startup based in Tallinn. Backed by TechStars, SAP, Jaan Tallinn, etc.<br></br> People-analytics (personality, etc.) for hiring and team-building.</div>
               <div className="role">UX/UI, Web, Visual</div>
             </div>
@@ -71,19 +71,19 @@ export default function Home() {
             <div className="experience">
               <div className="type">Freelance</div>
               <div className="spacetime">2 months (Nov 2019 – Dec 2020) · Remote</div>
-              <div className="name">Teamscope</div>
+              <div className="name"><a href="https://teamscope.io/" target="_blank">Teamscope</a></div>
               <div>Early-stage 6-person startup based in Tallinn. Backed by TechStars, SAP, Jaan Tallinn, etc.<br></br> People-analytics (personality, etc.) for hiring and team-building.</div>
               <div className="role">UX/UI</div>
             </div>
 
-            <div className="experience">
+            <div className="experience text">
               <div>Started searching for full-time role in mid 2018. Got an offer from IndieCampers, Lisbon-based biggest campervan rental provider in Europe. Then an offer from Teamscope, after which I moved to Tallinn, Estonia.</div>
             </div>
 
             <div className="experience">
               <div className="type">Freelance</div>
               <div className="spacetime">9 months (Jul 2018 – Mar 2019) · Remote</div>
-              <div className="name">Portal</div>
+              <div className="name"><a href="https://getportal.co/" target="_blank">Portal</a></div>
               <div>Early-stage 3-person startup based in San Francisco. Pre-seed.<br></br> Cryptocurrency wallet, transfering, exchange and conversion — all-in-one, for everyone.</div>
               <div className="role">UX/UI, Web, Visual</div>
             </div>
@@ -91,7 +91,7 @@ export default function Home() {
             <div className="experience">
               <div className="type">Freelance</div>
               <div className="spacetime">3 months (Nov 2017 – Dec 2017) · Remote</div>
-              <div className="name">Tides</div>
+              <div className="name">Tides by <a href="https://www.crunchbase.com/organization/fuel-5" target="_blank">Fuel</a></div>
               <div>Early-stage 3-person startup based in San Francisco. Pre-seed.<br></br> Decentralizing health insurance using blockchain technology.</div>
               <div className="role">Landing page, business card</div>
             </div>
@@ -99,7 +99,7 @@ export default function Home() {
             <div className="experience">
               <div className="type">Freelance</div>
               <div className="spacetime">3 months (Jun 2017 – Aug 2017) · Remote</div>
-              <div className="name">Meitu</div>
+              <div className="name"><a href="https://www.crunchbase.com/organization/meitu" target="_blank">Meitu</a></div>
               <div>Series-B startup based in China and US. Backed by Qiming Ventures, etc.<br></br> One of the leading providers of photo-editing apps on mobile.</div>
               <div className="role">Visual (infographics)</div>
             </div>
@@ -107,7 +107,7 @@ export default function Home() {
             <div className="experience">
               <div className="type">Freelance</div>
               <div className="spacetime">3 months (Feb 2017 – Apr 2017) · Remote</div>
-              <div className="name">Engine eCommerce</div>
+              <div className="name"><a href="https://www.crunchbase.com/organization/engine-ecommerce" target="_blank">Engine</a></div>
               <div>Early-stage startup based in US. Backed by fund for Arkansas' Future.<br></br> Create and manage a custom online shop.
 </div>
               <div className="role">UX/UI (admin panel)</div>
@@ -124,7 +124,7 @@ export default function Home() {
             <div className="experience">
               <div className="type">Freelance</div>
               <div className="spacetime">6 months (Jan 2016 – Jun 2016) · Remote</div>
-              <div className="name">Foodmunk</div>
+              <div className="name">Foodmunk by <a href="https://www.crunchbase.com/organization/fuel-5" target="_blank">Fuel</a></div>
               <div>Early-stage 3-person startup based in San Francisco. Backed by 500 Startups.<br></br> iOS-based nutrition assistant.</div>
               <div className="role">UX/UI</div>
             </div>
@@ -132,43 +132,43 @@ export default function Home() {
             <div className="experience">
               <div className="type">Freelance</div>
               <div className="spacetime">1 week (Oct 2015) · Remote</div>
-              <div className="name">MultiplyLabs</div>
+              <div className="name"><a href="https://multiplylabs.com/" target="_blank">MultiplyLabs</a></div>
               <div>Early-stage startup based in San Francisco, CA. Backed by YC, Pegasus Tech Ventures, etc.<br></br> Order a customizable pill with supplements for energy and health.</div>
               <div className="role">Landing page</div>
             </div>
 
-            <div className="experience">
+            <div className="experience text">
               <div>Difficulties obtaining visa to stay in US. Also, thought about studying Industrial Design, but after preparing and taking drawing classes, I realized I don't wish to move away from screen, images and digital. So, I just continued freelancing remotely for 3 years from Sarajevo, Bosnia and Herzegovina.</div>
             </div>
 
             <div className="experience">
               <div className="type">Freelance</div>
               <div className="spacetime">4 months (Jun 2015 – Sep 2015) · San Francisco, CA</div>
-              <div className="name">Kamcord</div>
+              <div className="name"><a href="https://medium.com/@Kamcord/were-joining-lyft-d1bb6523ac90" target="_blank">Kamcord</a></div>
               <div>Series-A 40-person startup based in San Francisco. Backed by YC, Tencent, etc.<br></br> Watch and record gameplays and livestreams of mobile games.</div>
               <div className="role">UX/UI, Web, Visual</div>
             </div>
 
-            <div className="experience">
+            <div className="experience text">
               <div>Disagreements with CEO that Branded.me should become a social network for professionals. Branded.me had 100k signups within 3 months of launch, and talks of acquisition from Amazon.</div>
             </div>
 
             <div className="experience">
               <div className="type">Employee</div>
               <div className="spacetime">9 months (Sep 2014 – May 2015) · San Francisco, CA</div>
-              <div className="name">Branded.me</div>
+              <div className="name"><a href="https://www.crunchbase.com/organization/branded-me" target="_blank">Branded.me</a></div>
               <div>Early-stage 5-person startup based in San Francisco. Pre-seed.<br></br> Create a personal website from scratch or using LinkedIn profile data.</div>
               <div className="role">UX/UI, Web, Visual</div>
             </div>
 
-            <div className="experience">
+            <div className="experience text">
               <div>High-school ended, I moved to San Francisco to work closely on Branded.me, the new venture of founder of Mobiley.</div>
             </div>
 
             <div className="experience">
               <div className="type">Freelance</div>
               <div className="spacetime">1 year (Aug 2013 – Aug 2014) · Remote</div>
-              <div className="name">Mobiley</div>
+              <div className="name"><a href="https://www.crunchbase.com/organization/mobiley" target="_blank">Mobiley</a></div>
               <div>Early-stage 2-person startup based in San Francisco. Became Branded.me.<br></br> Create a mobile version of the website from your existing website.</div>
               <div className="role">UX/UI, Web, Visual</div>
             </div>
@@ -176,7 +176,7 @@ export default function Home() {
             <div className="experience">
               <div className="type">Freelance</div>
               <div className="spacetime">1 year (Jul 2012 – Jul 2013) · Remote</div>
-              <div className="name">Weebly</div>
+              <div className="name"><a href="http://weebly.com/" target="_blank">Weebly</a></div>
               <div>Series-A startup based in San Francisco. Backed by YC, Sequoia, etc.<br></br> Create a website without coding.</div>
               <div className="role">Website templates</div>
             </div>
@@ -184,7 +184,7 @@ export default function Home() {
             <div className="experience">
               <div className="type">Freelance</div>
               <div className="spacetime">1 year (Jul 2012 – Jul 2013) · Remote</div>
-              <div className="name">Helpjuice</div>
+              <div className="name"><a href="http://helpjuice.com/" target="_blank">Helpjuice</a></div>
               <div>Early-stage startup based in Austin. Backed by TechStars.<br></br> Create a knowledge-base for external (help center) and internal use.</div>
               <div className="role">UX/UI, Web, Visual</div>
             </div>
@@ -192,12 +192,12 @@ export default function Home() {
             <div className="experience">
               <div className="type">Freelance</div>
               <div className="spacetime">1 year (Jul 2011 – Jul 2012) · Remote</div>
-              <div className="name">Playmonks</div>
+              <div className="name"><a href="https://www.crunchbase.com/organization/turbobotz" target="_blank">Playmonks</a></div>
               <div>Early-stage startup based in Chicago. Backed by The Brandery.<br></br> Find new PC/console games based on what you and your friends played.</div>
               <div className="role">UI, Web</div>
             </div>
 
-            <div>I started when I was 13 by posting my work of web design, graphic design and visual art on DeviantART and Dribbble, which led to freelance work, one of which was an early-stage startup, and I mostly continued working with startups.</div>
+            <div className="experience text last">I started when I was 13 by posting my work of web design, graphic design and visual art on DeviantART and Dribbble, which led to freelance work, one of which was an early-stage startup, and I mostly continued working with startups.</div>
 
           </div>
         </div>
