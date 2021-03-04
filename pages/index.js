96a98fb84ac3036ac7c5f0cff4eb1393d114368e
adis-hasanic <adisdesigner96@gmail.com>
2021-03-04 20:39:33 +0100

    test

--- pages/index.js
+++ pages/index.js
@@ -4,21 +4,9 @@
 export default function Home() {
   return (
 
-    <div className="landing">
-      <Head><title>Adis Hasanic</title><link rel="shortcut icon" href="favicon.jpg" />
-        <script async src="https://www.googletagmanager.com/gtag/js?id=UA-121629290-2"></script>
-        <script
-          dangerouslySetInnerHTML={{
-            __html: `
-  window.dataLayer = window.dataLayer || [];
-  function gtag(){dataLayer.push(arguments);}
-  gtag('js', new Date());
+    <div>
+     
 
-  gtag('config', 'UA-121629290-2');
-  `,
-          }}
-        /></Head>
-
-      <div></div>
+      <div>
   )
 }
