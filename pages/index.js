ddab5fcd9b38da4751c9bb3f94042a0b05bce466
adis-hasanic <adisdesigner96@gmail.com>
2021-03-06 21:21:31 +0100

    test

--- pages/index.js
+++ pages/index.js
@@ -5,7 +5,7 @@ export default function Home() {
   return (
 
     <div>
-      <Head><title>Adis Hasanic</title><link rel="shortcut icon" href="favicon.jpg" />
+      <Head><title>Adis Hasanic - Product Designer (UI/UX)</title><link rel="shortcut icon" href="favicon.jpg" />
         <script async src="https://www.googletagmanager.com/gtag/js?id=UA-121629290-2"></script>
         <script
           dangerouslySetInnerHTML={{
@@ -18,6 +18,9 @@ export default function Home() {
   `,
           }}
         /></Head>
+
+        <div>
+          Adis Hasanic<br></br><a href="https://adishasanic.com/Adis-resume.pdf">Download CV</a></div>
 
       </div>
   )
