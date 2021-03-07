59309ed392102bf567b0dcf2eb6dc081a416dfbd
adis-hasanic <adisdesigner96@gmail.com>
2021-03-07 15:06:00 +0100

    test

--- pages/thrive.js
+++ pages/thrive.js
@@ -22,131 +22,131 @@ export default function Home() {
       <div className="content presentation">
 
         <div className="row">
-          <img src="/portfolio/product/amigos/2/1.png" className="iphone-x"></img>
+          <img src="/portfolio/product/thrive/2/1.png" className="iphone-x"></img>
         </div>
 
         <div className="row">
-          <img src="/portfolio/product/amigos/2/2.png" className="iphone-x"></img>
+          <img src="/portfolio/product/thrive/2/2.png" className="iphone-x"></img>
         </div>
 
         <div className="row">
-          <img src="/portfolio/product/amigos/2/3.png" className="iphone-x"></img>
+          <img src="/portfolio/product/thrive/2/3.png" className="iphone-x"></img>
         </div>
 
         <div className="row">
-          <img src="/portfolio/product/amigos/2/4.png" className="iphone-x"></img>
+          <img src="/portfolio/product/thrive/2/4.png" className="iphone-x"></img>
         </div>
 
         <div className="row">
-          <img src="/portfolio/product/amigos/3/1.png" className="iphone-x"></img>
+          <img src="/portfolio/product/thrive/3/1.png" className="iphone-x"></img>
         </div>
 
         <div className="row">
-          <img src="/portfolio/product/amigos/3/2.png" className="iphone-x"></img>
+          <img src="/portfolio/product/thrive/3/2.png" className="iphone-x"></img>
         </div>
 
         <div className="row">
-          <img src="/portfolio/product/amigos/3/3.png" className="iphone-x"></img>
+          <img src="/portfolio/product/thrive/3/3.png" className="iphone-x"></img>
         </div>
 
         <div className="row">
-          <img src="/portfolio/product/amigos/3/4.png" className="iphone-x"></img>
+          <img src="/portfolio/product/thrive/3/4.png" className="iphone-x"></img>
         </div>
 
         <div className="row">
-          <img src="/portfolio/product/amigos/3/5.png" className="iphone-x"></img>
+          <img src="/portfolio/product/thrive/3/5.png" className="iphone-x"></img>
         </div>
 
         <div className="row">
-          <img src="/portfolio/product/amigos/4/1.png" className="iphone-x"></img>
+          <img src="/portfolio/product/thrive/4/1.png" className="iphone-x"></img>
         </div>
 
         <div className="row">
-          <img src="/portfolio/product/amigos/4/2.png" className="iphone-x"></img>
+          <img src="/portfolio/product/thrive/4/2.png" className="iphone-x"></img>
         </div>
 
         <div className="row">
-          <img src="/portfolio/product/amigos/4/3.png" className="iphone-x"></img>
+          <img src="/portfolio/product/thrive/4/3.png" className="iphone-x"></img>
         </div>
 
         <div className="row">
-          <img src="/portfolio/product/amigos/4/4.png" className="iphone-x"></img>
+          <img src="/portfolio/product/thrive/4/4.png" className="iphone-x"></img>
         </div>
 
         <div className="row">
-          <img src="/portfolio/product/amigos/5/1.png" className="iphone-x"></img>
+          <img src="/portfolio/product/thrive/5/1.png" className="iphone-x"></img>
         </div>
 
         <div className="row">
-          <img src="/portfolio/product/amigos/5/2.png" className="iphone-x"></img>
+          <img src="/portfolio/product/thrive/5/2.png" className="iphone-x"></img>
         </div>
 
         <div className="row">
-          <img src="/portfolio/product/amigos/5/3.png" className="iphone-x"></img>
+          <img src="/portfolio/product/thrive/5/3.png" className="iphone-x"></img>
         </div>
 
         <div className="row">
-          <img src="/portfolio/product/amigos/5/4.png" className="iphone-x"></img>
+          <img src="/portfolio/product/thrive/5/4.png" className="iphone-x"></img>
         </div>
 
         <div className="row">
-          <img src="/portfolio/product/amigos/5/5.png" className="iphone-x"></img>
+          <img src="/portfolio/product/thrive/5/5.png" className="iphone-x"></img>
         </div>
 
         <div className="row">
-          <img src="/portfolio/product/amigos/5/6.png" className="iphone-x"></img>
+          <img src="/portfolio/product/thrive/5/6.png" className="iphone-x"></img>
         </div>
 
         <div className="row">
-          <img src="/portfolio/product/amigos/7/5.png" className="iphone-x-landscape"></img>
+          <img src="/portfolio/product/thrive/7/5.png" className="iphone-x-landscape"></img>
         </div>
 
         <div className="row">
-          <img src="/portfolio/product/amigos/7/6.png" className="iphone-x-landscape"></img>
+          <img src="/portfolio/product/thrive/7/6.png" className="iphone-x-landscape"></img>
         </div>
 
         <div className="row">
-          <img src="/portfolio/product/amigos/7/7.png" className="iphone-x-landscape"></img>
+          <img src="/portfolio/product/thrive/7/7.png" className="iphone-x-landscape"></img>
         </div>
 
         <div className="row">
-          <img src="/portfolio/product/amigos/7/8.png" className="iphone-x"></img>
+          <img src="/portfolio/product/thrive/7/8.png" className="iphone-x"></img>
         </div>
 
         <div className="row">
-          <img src="/portfolio/product/amigos/7/9.png" className="iphone-x"></img>
+          <img src="/portfolio/product/thrive/7/9.png" className="iphone-x"></img>
         </div>
 
         <div className="row">
-          <img src="/portfolio/product/amigos/7/10.png" className="iphone-x"></img>
+          <img src="/portfolio/product/thrive/7/10.png" className="iphone-x"></img>
         </div>
 
         <div className="row">
-          <img src="/portfolio/product/amigos/7/11.png" className="iphone-x"></img>
+          <img src="/portfolio/product/thrive/7/11.png" className="iphone-x"></img>
         </div>
 
         <div className="row">
-          <img src="/portfolio/product/amigos/7/12.png" className="iphone-x"></img>
+          <img src="/portfolio/product/thrive/7/12.png" className="iphone-x"></img>
         </div>
 
         <div className="row">
-          <img src="/portfolio/product/amigos/7/13.png" className="iphone-x"></img>
+          <img src="/portfolio/product/thrive/7/13.png" className="iphone-x"></img>
         </div>
 
         <div className="row">
-          <img src="/portfolio/product/amigos/8/1.png" className="iphone-x"></img>
+          <img src="/portfolio/product/thrive/8/1.png" className="iphone-x"></img>
         </div>
 
         <div className="row">
-          <img src="/portfolio/product/amigos/8/2.png" className="iphone-x"></img>
+          <img src="/portfolio/product/thrive/8/2.png" className="iphone-x"></img>
         </div>
 
         <div className="row">
-          <img src="/portfolio/product/amigos/8/3.png" className="iphone-x"></img>
+          <img src="/portfolio/product/thrive/8/3.png" className="iphone-x"></img>
         </div>
 
         <div className="row">
-          <img src="/portfolio/product/amigos/8/1.png" className="iphone-x"></img>
+          <img src="/portfolio/product/thrive/8/1.png" className="iphone-x"></img>
         </div>
 
       </div>
