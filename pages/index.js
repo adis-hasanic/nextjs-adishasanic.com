522e8eb71ba18c3f5cdb4558e6db87aa9c5fb226
adis-hasanic <adisdesigner96@gmail.com>
2021-03-07 15:41:06 +0100

    test

--- pages/index.js
+++ pages/index.js
@@ -106,9 +106,10 @@ export default function Home() {
           <img src="/portfolio/product/kamcord/broadcasting/4.png" className="android-landscape"></img><br></br>
         </div>
 
-        <div className="row">
-          <h1>Kamcord Livestream Watching</h1><br></br>
-          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p><br></br>
+        <div className="row live">
+          <h2>Kamcord Livestream Watching</h2><br></br>
+          <p>Watching a livestream on phone of a mobile game. Makes it easy to send positive reactions, chat, connect, and explore who else is watching.</p><br></br>
+          <Link href="/kamcord-livestream-watching"><a href="/kamcord-livestream-watching">See more</a></Link><br></br>
           <img src="/portfolio/product/kamcord/livestream/2.png" className="iphone-five"></img><br></br>
         </div>
 
