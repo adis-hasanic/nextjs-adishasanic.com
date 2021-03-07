4911be46651f587227d1dd8713c37697bb903174
adis-hasanic <adisdesigner96@gmail.com>
2021-03-07 17:25:27 +0100

    test

--- pages/flowdash.js
+++ pages/flowdash.js
@@ -46,11 +46,75 @@ export default function Home() {
         </div>
 
         <div className="row">
+          <img src="/portfolio/product/flowdash/table-3.jpg" className="eleven-twenty border"></img>
+        </div>
+
+        <div className="row">
+          <img src="/portfolio/product/flowdash/table-4.jpg" className="eleven-twenty border"></img>
+        </div>
+
+        <div className="row">
+          <img src="/portfolio/product/flowdash/comments-1.jpg" className="eleven-twenty border"></img>
+        </div>
+
+        <div className="row">
+          <img src="/portfolio/product/flowdash/comments-2.jpg" className="eleven-twenty border"></img>
+        </div>
+
+        <div className="row">
+          <img src="/portfolio/product/flowdash/filters-1.jpg" className="eleven-twenty border"></img>
+        </div>
+
+        <div className="row">
+          <img src="/portfolio/product/flowdash/detail-1.jpg" className="eleven-twenty border"></img>
+        </div>
+
+        <div className="row">
+          <img src="/portfolio/product/flowdash/detail-2.jpg" className="eleven-twenty border"></img>
+        </div>
+
+        <div className="row">
+          <img src="/portfolio/product/flowdash/flow-0-1.jpg" className="eleven-twenty border"></img>
+        </div>
+
+        <div className="row">
+          <img src="/portfolio/product/flowdash/flow-0-2.jpg" className="eleven-twenty border"></img>
+        </div>
+
+        <div className="row">
+          <img src="/portfolio/product/flowdash/flow-1.jpg" className="eleven-twenty border"></img>
+        </div>
+
+        <div className="row">
+          <img src="/portfolio/product/flowdash/flow-2.jpg" className="eleven-twenty border"></img>
+        </div>
+
+        <div className="row">
+          <img src="/portfolio/product/flowdash/flow-3.jpg" className="eleven-twenty border"></img>
+        </div>
+
+        <div className="row">
           <img src="/portfolio/product/flowdash/settings-1.jpg" className="eleven-twenty border"></img>
         </div>
 
         <div className="row no-vh">
           <img src="/portfolio/product/flowdash/settings-2.jpg" className="eleven-twenty border"></img>
+        </div>
+
+        <div className="row no-vh">
+          <img src="/portfolio/product/flowdash/settings-3.jpg" className="eleven-twenty border"></img>
+        </div>
+
+        <div className="row no-vh">
+          <img src="/portfolio/product/flowdash/collaborators-1.jpg" className="eleven-twenty border"></img>
+        </div>
+
+        <div className="row no-vh">
+          <img src="/portfolio/product/flowdash/permissions-1.jpg" className="eleven-twenty border"></img>
+        </div>
+
+        <div className="row no-vh">
+          <img src="/portfolio/product/flowdash/timers-1.jpg" className="eleven-twenty border"></img>
         </div>
 
       </div>
