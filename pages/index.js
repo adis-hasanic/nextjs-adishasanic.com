50f6e7f6a8961fb5524d03fcc7b7a2aedf666c0e
adis-hasanic <adisdesigner96@gmail.com>
2021-03-08 20:13:58 +0100

    test

--- pages/index.js
+++ pages/index.js
@@ -41,83 +41,84 @@ export default function Home() {
 
         <div className="row live">
           <h2>Teamscope</h2><br></br>
-          <p>Enterprise software that uses surveys and machine learning to analyze personalities, values and competencies, to help companies learn about their people to build better teams.</p><br></br>
+          <p>Learn about personality and values to build teams.</p><br></br>
           <Link href="/teamscope"><a href="/teamscope">See more</a></Link><br></br>
           <img src="/portfolio/product/teamscope/1/2.jpg" className="twelve-eighty border"></img><br></br>
         </div>
 
         <div className="row live">
           <h2>Thrive</h2><br></br>
-          <p>Vehicle marketplace focusing on helping the customer find the car they want and which is available at a nearby dealership.</p><br></br>
+          <p>Buy a car you want at a dealership nearby.</p><br></br>
           <Link href="/thrive"><a href="/thrive">See more</a></Link><br></br>
           <img src="/portfolio/product/thrive/2/1.png" className="iphone-x"></img><br></br>
         </div>
 
         <div className="row live">
           <h2>Portal</h2><br></br>
-          <p>Mac app that makes it easy for anyone to store, transfer, exchange and convert cryptocurrencies, and helps trade and withdraw on time.</p><br></br>
+          <p>Store, transfer and exchange cryptocurrencies.</p><br></br>
           <Link href="/portal"><a href="/portal">See more</a></Link><br></br>
           <img src="/portfolio/product/portal/2/1.jpg" className="twelve-eighty"></img><br></br>
         </div>
 
         <div className="row live">
           <h2>Typen</h2><br></br>
-          <p>Web-based writing app that helps focus on writing by providing text storage, customizable look, distraction-free mode.</p><br></br>
+          <p>Focus on writing.</p><br></br>
           <Link href="/typen"><a href="/typen">See more</a></Link><br></br>
           <img src="/portfolio/product/typen/1.png" className="eleven-ninetysix"></img><br></br>
         </div>
 
         <div className="row live">
           <h2>Engine</h2><br></br>
-          <p>Admin panel of a web platform that enables anyone to set up and run an online store, independently on a custom domain.</p><br></br>
+          <p>Manage an online store.</p><br></br>
           <Link href="/engine"><a href="/engine">See more</a></Link><br></br>
           <img src="/portfolio/product/engine/2.jpg" className="twelve-eighty"></img><br></br>
         </div>
 
         <div className="row live">
           <h2>LiftIgniter</h2><br></br>
-          <p>Setting up and managing an API that when integrated learns about app's users to show accurate content recommendations.</p><br></br>
+          <p>Setup API for content recommendations.</p><br></br>
           <Link href="/liftigniter"><a href="/liftigniter">See more</a></Link><br></br>
           <img src="/portfolio/product/liftigniter/4.jpg" className="twelve-eighty"></img><br></br>
         </div>
 
         <div className="row live">
-          <h2>IndieCampers Campervan Booking</h2><br></br>
-          <p>Process of booking a campervan via mobile website of IndieCampers, Europe's largest campervan rental service.</p><br></br>
+          <h2>IndieCampers Booking</h2><br></br>
+          <p>Book a campervan via mobile website.</p><br></br>
           <Link href="/indiecampers"><a href="/indiecampers">See more</a></Link><br></br>
           <img src="/portfolio/product/indiecampers/preview.png" className="iphone-five"></img><br></br>
         </div>
 
         <div className="row live">
           <h2>Foodmunk</h2><br></br>
-          <p>Nutrition assistant that learns about user to ensure that each next meal is the best choice for health and variety (based on their previous meals).</p><br></br>
+          <p>Find the next best meal for you nearby.</p><br></br>
           <Link href="/foodmunk"><a href="/foodmunk">See more</a></Link><br></br>
           <img src="/portfolio/product/foodmunk/3/1.png" className="iphone-five"></img><br></br>
         </div>
 
         <div className="row live">
-          <h2>Kamcord Livestream Broadcasting</h2><br></br>
-          <p>The solution that didn't exist until then — ability to host a livestream of a mobile game, using only a phone while playing a mobile game on it.</p><br></br>
+          <h2>Kamcord Broadcasting</h2><br></br>
+          <p>Host a livestream while playing a mobile game.</p><br></br>
           <Link href="/kamcord-livestream-broadcasting"><a href="/kamcord-livestream-broadcasting">See more</a></Link><br></br>
           <img src="/portfolio/product/kamcord/broadcasting/4.png" className="android-landscape"></img><br></br>
         </div>
 
         <div className="row live">
-          <h2>Kamcord Livestream Watching</h2><br></br>
-          <p>Watching a livestream on phone of a mobile game. Makes it easy to send positive reactions, chat, connect, and explore who else is watching.</p><br></br>
+          <h2>Kamcord Livestream</h2><br></br>
+          <p>Watch a livestream of a mobile game.</p><br></br>
           <Link href="/kamcord-livestream-watching"><a href="/kamcord-livestream-watching">See more</a></Link><br></br>
           <img src="/portfolio/product/kamcord/livestream/2.png" className="iphone-five"></img><br></br>
         </div>
 
         <div className="row">
           <h1>Kamcord.com</h1><br></br>
-          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p><br></br>
+          <p>Find gameplays and livestreams, and discover new games.</p><br></br>
+          <Link href="/kamcord-website"><a href="/kamcord-livestream-watching">See more</a></Link><br></br>
           <img src="/portfolio/product/kamcord/website/1.jpg" className="twelve-eighty"></img><br></br>
         </div>
 
         <div className="row live">
           <h1>Branded.me</h1><br></br>
-          <p>Enabling anyone to create a website about their professional history and achievements, focusing on converting LinkedIn profile into website.</p><br></br>
+          <p>Create a personal website, without coding, in minutes.</p><br></br>
           <Link href="/brandedme"><a href="/brandedme">See more</a></Link><br></br>
           <img src="/portfolio/product/brandedme/1.jpg" className="eleven-twenty"></img><br></br>
         </div>
