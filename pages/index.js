dd8b1a47f9948723441114e9178915c23708424d
adis-hasanic <adisdesigner96@gmail.com>
2021-03-09 11:26:45 +0100

    test

--- pages/index.js
+++ pages/index.js
@@ -28,7 +28,7 @@ export default function Home() {
         <div className="row live">
           <h1>Flowdash</h1><br></br>
           <h2>Create web apps, without coding, for team workflows.</h2><br></br>
-          <p>Flowdash was a 2-person startup (software engineers who are also building the product). I was designing at Flowdash for 4 months, making it easy to build and use workflows in Flowdash, as well as designing new features and capabilities into the product. After 4 months since I started, new version of Flowdash was posted on ProductHunt and it became #2 product of the month with more than 2000 upvotes.</p><br></br>
+          <p>Flowdash was a San Francsico based 2-person startup, backed by YC. I was designing at Flowdash for 4 months (Aug 2020 - Dec 2020), making it easy to build and use workflows in Flowdash, as well as designing new features and capabilities into the product. After 4 months since I started, new version of Flowdash was posted on ProductHunt and it became #2 product of the month with more than 2000 upvotes.</p><br></br>
           <Link href="/flowdash"><a href="/flowdash">See more</a></Link><br></br>
           <img src="/portfolio/product/flowdash/flow-2.jpg" className="eleven-twenty border"></img><br></br>
         </div>
@@ -36,7 +36,7 @@ export default function Home() {
         <div className="row live">
           <h1>Stacker</h1><br></br>
           <h2>Create web apps, without coding, from spreadsheets.</h2><br></br>
-          <p>Stacker was a 5-person startup. I was designing at Stacker for 4 months, starting from making quick visual changes to their current UI, to making the editor easy to use, setup easy to understand, and to designing a new version of the product that focuses on workspaces and end-user productivity more than stand-alone apps.</p><br></br>
+          <p>Stacker was a London based 5-person startup. I was designing at Stacker for 4 months (Feb 2020 - May 2020), starting from making quick visual changes to their current UI, to making the editor easy to use, setup easy to understand, and to designing a new version of the product that focuses on workspaces and end-user productivity more than stand-alone apps.</p><br></br>
           <Link href="/stacker"><a href="/stacker">See more</a></Link><br></br>
           <img src="/portfolio/product/stacker/7/2.jpg" className="twelve-eighty"></img><br></br>
         </div>
@@ -44,7 +44,7 @@ export default function Home() {
         <div className="row live">
           <h1>Teamscope</h1><br></br>
           <h2>Analyze personality and values to build teams.</h2><br></br>
-          <p>Teamscope was a 6-person startup. I was a sole designer at Teamscope for 1 year, working most closely with CEO and PM to design a product which presents an easy-to-follow process to the employees and candidates who will take the tests, as well as team builders and hiring managers to understand data via visualizations and find applicable insights.</p><br></br>
+          <p>Teamscope was a Tallin based 6-person startup, backed by TechStars. I was a sole designer at Teamscope for 1 year (Nov 2018 - Jan 2020), working most closely with CEO and PM to design a product which presents an easy-to-follow process to the employees and candidates who will take the tests, as well as team builders and hiring managers to understand data via visualizations and find applicable insights.</p><br></br>
           <Link href="/teamscope"><a href="/teamscope">See more</a></Link><br></br>
           <img src="/portfolio/product/teamscope/1/2.jpg" className="twelve-eighty border"></img><br></br>
         </div>
@@ -52,7 +52,7 @@ export default function Home() {
         <div className="row live">
           <h1>Thrive</h1><br></br>
           <h2>Buy a car you want at a dealership nearby.</h2><br></br>
-          <p>Entire app designed in about 1 month. Started with only the idea from the founder that it should be a marketplace designed as a social network that allows you to like, revisit, check comments and talk to dealerships. Result was a car-search focused platform designed as a funnel from exploration to narrowing down to favorites to talking with dealerships in purchasing.</p><br></br>
+          <p>Entire app designed in about 1 month (Sep 2019). Started with only the idea from the founder that it should be a marketplace designed as a social network that allows you to like, revisit, check comments and talk to dealerships. Result was a car-search focused platform designed as a funnel from exploration to narrowing down to favorites to talking with dealerships in purchasing.</p><br></br>
           <Link href="/thrive"><a href="/thrive">See more</a></Link><br></br>
           <img src="/portfolio/product/thrive/2/1.png" className="iphone-x"></img><br></br>
         </div>
@@ -60,9 +60,17 @@ export default function Home() {
         <div className="row live">
           <h1>Portal</h1><br></br>
           <h2>Store, transfer and exchange cryptocurrencies.</h2><br></br>
-          <p>Portal was a 3-person startup. I was designing Portal for 9 months, working closely with CEO and CTO to understand the complex processes of working with cryptocurrencies and designing a single app where user can track and manage all their crypto finances and crypto related activities.</p><br></br>
+          <p>Portal was a San Francisco based 3-person startup. Founders' previous venture was backed by 500 Startups. I was designing Portal for 9 months (mid 2018 - early 2019), working closely with CEO and CTO to understand the complex processes of working with cryptocurrencies and designing a single app where user can track and manage all their crypto finances and crypto related activities.</p><br></br>
           <Link href="/portal"><a href="/portal">See more</a></Link><br></br>
           <img src="/portfolio/product/portal/2/1.jpg" className="twelve-eighty"></img><br></br>
+        </div>
+
+        <div className="row live">
+          <h1>IndieCampers Booking</h1><br></br>
+          <h2>Book a campervan via mobile website.</h2><br></br>
+          <p>IndieCampers was a Lisbon-based early-stage startup, and biggest campervan rental service in Europe. I was given 1 week (in Oct 2018) to design UI and UX of a lengthy process of booking a campervan via mobile website. After total 8 hours of work, I turned 5 pages into 1, simplified steps and language, created a new visual style, and sent it via email along with my reasoning behind all the design decisions. CEO and PM were impressed and offered me a full-time role at IndieCampers.</p><br></br>
+          <Link href="/indiecampers"><a href="/indiecampers">See more</a></Link><br></br>
+          <img src="/portfolio/product/indiecampers/preview.png" className="iphone-five"></img><br></br>
         </div>
 
         <div className="row live">
@@ -87,16 +95,9 @@ export default function Home() {
         </div>
 
         <div className="row live">
-          <h1>IndieCampers Booking</h1><br></br>
-          <h2>Book a campervan via mobile website.</h2><br></br>
-          <p>IndieCampers was a startup, and biggest campervan rental service in Europe. I was given 1 week to design UI and UX of a lengthy process of booking a campervan via mobile website. After total 8 hours of work, I turned 5 pages into 1, simplified steps and language, created a new visual style, and sent it via email along with my reasoning behind all the design decisions. CEO and PM were impressed and offered me a full-time role at IndieCampers.</p><br></br>
-          <Link href="/indiecampers"><a href="/indiecampers">See more</a></Link><br></br>
-          <img src="/portfolio/product/indiecampers/preview.png" className="iphone-five"></img><br></br>
-        </div>
-
-        <div className="row live">
-          <h2>Foodmunk</h2><br></br>
-          <p>Find the next best meal for you nearby.</p><br></br>
+          <h1>Foodmunk</h1><br></br>
+          <h2>Find the next best meal for you nearby.</h2><br></br>
+          <p>Foodmunk was an app by a 3-person digital health startup called Fuel, backed by 500 Startups. In about 6 months (Jan 2016 - Jun 2016), working closely with CEO, I designed UI and UX of an iOS app that learns about user's body, lifestyle and choices, to be able to recommend the next best meal nearby. The best means not only that it's healthy, but that it also provides variety when compared to previous choices.</p><br></br>
           <Link href="/foodmunk"><a href="/foodmunk">See more</a></Link><br></br>
           <img src="/portfolio/product/foodmunk/3/1.png" className="iphone-five"></img><br></br>
         </div>
@@ -104,7 +105,7 @@ export default function Home() {
         <div className="row live">
           <h1>Kamcord Broadcasting</h1><br></br>
           <h2>Host a livestream while playing a mobile game.</h2><br></br>
-          <p>Kamcord was Series A startup with more than 40 people. I was working on Kamcord Broadcasting for 3 weeks, under a Senior Designer, who defined product requirements with PM and founders, and assigned me to design UI and UX, providing only the idea/direction that it could work similarly to Messenger floating avatars as a proven interaction pattern that helps in multitasking on mobile.</p><br></br>
+          <p>Kamcord was San Francisco based startup with more than 40 people, backed by YC, Tencent, etc. I was working on Kamcord Broadcasting for 3 weeks (mid 2015), under a Senior Designer, who defined product requirements with PM and founders, and assigned me to design UI and UX, providing only the idea/direction that it could work similarly to Messenger floating avatars as a proven interaction pattern that helps in multitasking on mobile.</p><br></br>
           <Link href="/kamcord-livestream-broadcasting"><a href="/kamcord-livestream-broadcasting">See more</a></Link><br></br>
           <img src="/portfolio/product/kamcord/broadcasting/4.png" className="android-landscape"></img><br></br>
         </div>
