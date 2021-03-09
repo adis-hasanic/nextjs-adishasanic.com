db99f208e3ff632a1fa24c963c218600d6c059d7
adis-hasanic <adisdesigner96@gmail.com>
2021-03-09 10:13:56 +0100

    test

--- pages/index.js
+++ pages/index.js
@@ -28,7 +28,7 @@ export default function Home() {
         <div className="row live">
           <h1>Flowdash</h1><br></br>
           <h2>Create web apps, without coding, for team workflows.</h2><br></br>
-          <p>Flowdash was a 2-person startup (software engineers who are also building the product). I was designing at Flowdash for 4 months, making it easy to build and use workflows in Flowdash, as well as designing new features and capabilities into the product. New version launched on ProductHunt after 4 months was #2 product of the month with more than 2000 upvotes.</p><br></br>
+          <p>Flowdash was a 2-person startup (software engineers who are also building the product). I was designing at Flowdash for 4 months, making it easy to build and use workflows in Flowdash, as well as designing new features and capabilities into the product. After 4 months since I started, new version of Flowdash was posted on ProductHunt and it became #2 product of the month with more than 2000 upvotes.</p><br></br>
           <Link href="/flowdash"><a href="/flowdash">See more</a></Link><br></br>
           <img src="/portfolio/product/flowdash/flow-2.jpg" className="eleven-twenty border"></img><br></br>
         </div>
