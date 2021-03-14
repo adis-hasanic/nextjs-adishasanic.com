0953ba68a4911f51aa0e59ca429cd7fa3850fd16
adis-hasanic <adisdesigner96@gmail.com>
2021-03-14 21:45:37 +0100

    test

---
+++ pages/references.js
@@ -0,0 +1,142 @@
+
+
+export default function Home() {
+  return (
+    <div>
+      <div className="content references">
+        <div className="row">
+          <div className="reference">
+            <img src="/references/chandra-duggirala.jpg"></img>
+            <div className="text"><p>“Adis does top quality design work and is fast and very responsive to client's needs. I strongly recommend him without any reservations.”</p>
+              <div className="person">
+                <span className="name">Chandra Duggirala</span>
+                <a href="https://www.linkedin.com/in/csentropy" target="_blank"><img src="linkedin.svg"></img></a>
+                <span className="about">co-founder/CEO of Portal (2019), Tides (2018) and Foodmunk (2016)</span>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="row">
+          <div className="reference">
+            <img src="/references/blake-puryear.jpg"></img>
+            <div className="text"><p>"From day one, Adis was able to massively contribute to the UX of our platform. He thoughtfully approaches his design decisions and takes the time to understand the individual problems of each user flow. He is always quick to respond and has created a visual design for our product that we are extremely happy with!"</p>
+              <div className="person">
+                <span className="name">Blake Puryear</span>
+                <a href="https://www.linkedin.com/in/bpuryear" target="_blank"><img src="linkedin.svg"></img></a>
+                <span className="about">while Product Lead at Engine in 2018</span>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="row">
+          <div className="reference">
+            <img src="/references/kelsey-klein.jpg"></img>
+            <div className="text"><p>"Adis took direction well, and was comfortable iterating [on infographics] through completion to create exactly what we were looking for."</p>
+              <div className="person">
+                <span className="name">Kelsey Klein</span>
+                <a href="https://www.linkedin.com/in/kelseyklein/" target="_blank"><img src="linkedin.svg"></img></a>
+                <span className="about">while PM at Meitu in 2017</span>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="row">
+          <div className="reference">
+            <img src="/references/indraneel-mukherjee.jpg"></img>
+            <div className="text"><p>“Adis is an excellent communicator, creates great designs fast, and is really great to work with. He quickly understood our rather complex requirements, did a bunch of quick iterations producing great work, was very flexible and patient as we came up with new requests, and also helped shape the overall product we were building."</p>
+              <div className="person">
+                <span className="name">Indraneel Mukherjee</span>
+                <a href="https://www.linkedin.com/in/indraneelmukherjee" target="_blank"><img src="linkedin.svg"></img></a>
+                <span className="about">while co-founder/CEO at LiftIgniter in 2016</span>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="row">
+          <div className="reference">
+            <img src="/references/satyam-singh.jpg"></img>
+            <div className="text"><p>“Adis hired me as a developer for his product "Typen". He communicated very well and co-ordinated with me whenever needed. He has good design skills that helped us in building the application. I really liked working with him and would be happy to work with him again."</p>
+              <div className="person">
+                <span className="name">Satyam Singh</span>
+                <span className="about">while full-stack developer on Typen in 2016</span>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="row">
+          <div className="reference">
+            <img src="/references/tiffany-kuo.jpg"></img>
+            <div className="text"><p>“Adis is easy to work with and fast. After sharing who our target customers are, a sketch of our landing page, and our copy, he was able to make suggestions to our original thinking and create a beautiful [landing page] design in just three days.”</p>
+              <div className="person">
+                <span className="name">Tiffany Kuo</span>
+                <a href="https://www.linkedin.com/in/tiffanyamykuo/" target="_blank"><img src="linkedin.svg"></img></a>
+                <span className="about">while co-founder at MultiplyLabs in 2015</span>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="row">
+          <div className="reference">
+            <img src="/references/gordon-tindall.jpg"></img>
+            <div className="text"><p>“Adis’ opus at Kamcord was establishing the user interface, visual design and interactions for the Android Broadcasting app. He communicates his thought process well and continually strives to better understand his users.”</p>
+              <div className="person">
+                <span className="name">Gordon Tindall</span>
+                <a href="https://www.linkedin.com/in/gordon-tindall-079b4416" target="_blank"><img src="linkedin.svg"></img></a>
+                <span className="about">while Senior Product Designer at Kamcord in 2015</span>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="row">
+          <div className="reference">
+            <img src="/references/stan-janssen.jpg"></img>
+            <div className="text"><p>“We set up and worked on an online writing platform for professionals. Adis came up with good ideas, maintained contact with our focus group and was timely with his deadlines.”</p>
+              <div className="person">
+                <span className="name">Stan Janssen</span>
+                <a href="https://www.linkedin.com/in/janssenstan" target="_blank"><img src="linkedin.svg"></img></a>
+                <span className="about">while freelance developer in 2014</span>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="row">
+          <div className="reference">
+            <img src="/references/dawson-whitfield.jpg"></img>
+            <div className="text"><p>"Adis was always able to deliver something new and supremely beautiful. He was key in exploring new UIs and visual designs for our 10M+ users. A true pleasure to work with (and you get kickass designs too).”</p>
+              <div className="person">
+                <span className="name">Dawson Whitfield</span>
+                <a href="https://ca.linkedin.com/in/dawsonwhitfield" target="_blank"><img src="linkedin.svg"></img></a>
+                <span className="about">while designer at Weebly in 2013</span>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="row">
+          <div className="reference">
+            <img src="/references/sandro-dzneladze.jpg"></img>
+            <div className="text"><p>“I consider two aspects important when working on a website design - communication and precision. Adis excelled at both. He always responded in a timely manner, and the final work was more than I expected.”</p>
+              <div className="person">
+                <span className="name">Sandro Dzneladze</span>
+                <a href="https://www.linkedin.com/in/sandro-dzneladze-a51b84a0" target="_blank"><img src="linkedin.svg"></img></a>
+                <span className="about">while freelance developer and photographer in 2012</span>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="row">
+          <div className="reference">
+            <img src="/references/vincent-chou.jpg"></img>
+            <div className="text"><p>“Adis produces nothing short of grade A work with great attention to detail and mindfulness of client requirements.”</p>
+              <div className="person">
+                <span className="name">Vincent Chou</span>
+                <a href="https://www.linkedin.com/in/vincentchou1" target="_blank"><img src="linkedin.svg"></img></a>
+                <span className="about">while founder/CEO at Playmonks in 2011</span>
+              </div>
+            </div>
+          </div>
+        </div>
+
+      </div>
+    </div>
+  )
+}
