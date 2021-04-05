392cd5968637e957a5d12506d493f91b0318dabb
adis-hasanic <adisdesigner96@gmail.com>
2021-04-06 01:37:40 +0200

    test

--- pages/flowdash.js
+++ pages/flowdash.js
@@ -28,6 +28,39 @@ export default function Home() {
         <div className="row">
           <img src="/portfolio/product/flowdash/table-4.jpg" className="eleven-twenty"></img>
         </div>
+        <div className="row">
+          <img src="/portfolio/product/flowdash/flow-0-1.jpg" className="eleven-twenty"></img>
+        </div>
+        <div className="row">
+          <img src="/portfolio/product/flowdash/flow-0-2.jpg" className="eleven-twenty"></img>
+        </div>
+        <div className="row">
+          <img src="/portfolio/product/flowdash/flow-1.jpg" className="eleven-twenty"></img>
+        </div>
+        <div className="row">
+          <img src="/portfolio/product/flowdash/flow-2.jpg" className="eleven-twenty"></img>
+        </div>
+        <div className="row">
+          <img src="/portfolio/product/flowdash/flow-3.jpg" className="eleven-twenty"></img>
+        </div>
+        <div className="row">
+          <img src="/portfolio/product/flowdash/detail-1.jpg" className="eleven-twenty"></img>
+        </div>
+        <div className="row">
+          <img src="/portfolio/product/flowdash/detail-2.jpg" className="eleven-twenty"></img>
+        </div>
+        <div className="row">
+          <img src="/portfolio/product/flowdash/settings-1.jpg" className="eleven-twenty"></img>
+        </div>
+        <div className="row">
+          <img src="/portfolio/product/flowdash/settings-2.jpg" className="eleven-twenty"></img>
+        </div>
+        <div className="row">
+          <img src="/portfolio/product/flowdash/settings-3.jpg" className="eleven-twenty"></img>
+        </div>
+        <div className="row">
+          <img src="/portfolio/product/flowdash/timers-1.jpg" className="eleven-twenty"></img>
+        </div>
       </div>
     </div>
   )
