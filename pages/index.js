b6ac743f7616daccff5ea780f2a10fc111bb6ff6
adis-hasanic <adisdesigner96@gmail.com>
2021-04-23 22:05:09 +0300

    test

--- pages/index.js
+++ pages/index.js
@@ -20,46 +20,13 @@ export default function Home() {
       <div className="me">
         <div>
         <div className="font-200-bold">Adis Hasanic</div>
-        <div className="font-100">UX and UI Designer</div>
         <div className="icons">
           <div><a href="https://www.linkedin.com/in/adis-hasanic/"><img src="linkedin.svg"></img></a></div>
           <div><a href="https://adishasanic96.typeform.com/to/IVmKhGC5"><img src="email.svg"></img></a></div>
         </div>
         </div>
       </div>
-      <div className="work">
-        <div>
-        <div className="font-300-bold title">Flowdash</div>
-        <div className="font-100 description">Enabling anyone to prepare a workflow for any size of team and any complexity of a business process.</div>
-        <div className="arrows">
-        <Link href=""><div className="inactive"><img src="arrow-left-web.svg"></img></div></Link>
-          <Link href="stacker"><div><img src="arrow-right-web.svg"></img></div></Link>
-        </div>
-        </div>
-      </div>
-
-      <div className="arrows-mobile">
-      <Link href=""><div className="inactive"><img src="arrow-left-mobile.svg"></img></div></Link>
-          <Link href="stacker"><div><img src="arrow-right-mobile.svg"></img></div></Link>
-        </div>
-
-      <div className="presentation twelve-eighty">
-      <div><img src="portfolio/product/flowdash/5.jpg"></img></div>
-      <div><img src="portfolio/product/flowdash/4.jpg"></img></div>
-      <div><img src="portfolio/product/flowdash/1.jpg"></img></div>
-      <div><img src="portfolio/product/flowdash/2.jpg"></img></div>
-      <div><img src="portfolio/product/flowdash/3.jpg"></img></div>
-      <div><img src="portfolio/product/flowdash/4-1.jpg"></img></div>
-      <div><img src="portfolio/product/flowdash/4-2.jpg"></img></div>
-      <div><img src="portfolio/product/flowdash/7-1.jpg"></img></div>
-      <div><img src="portfolio/product/flowdash/7-2.jpg"></img></div>
-      <div><img src="portfolio/product/flowdash/7-3.jpg"></img></div>
-      <div><img src="portfolio/product/flowdash/8.jpg"></img></div>
-        <div><img src="portfolio/product/flowdash/6.jpg"></img></div>
-        <div><img src="portfolio/product/flowdash/6-1.jpg"></img></div>
-        <div><img src="portfolio/product/flowdash/6-2.jpg"></img></div>
-        <div><img src="portfolio/product/flowdash/6-3.jpg"></img></div>
-      </div>
+      
     </div>
   )
 }
