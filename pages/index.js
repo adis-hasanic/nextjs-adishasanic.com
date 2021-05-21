5d5895e7e3ff1eeeebf94fa51908a4df829b9265
adis-hasanic <adisdesigner96@gmail.com>
2021-05-22 01:36:38 +0300

    test

--- pages/index.js
+++ pages/index.js
@@ -17,47 +17,210 @@ export default function Home() {
   `,
           }}
         /></Head>
-      <div className="me">
-        <div>
-        <div className="font-200-bold">Adis Hasanic</div>
-       <div className="font-100">UX, UI, Web and Visual</div>
-        <div className="icons">
-      <div><a href="https://adishasanic96.typeform.com/to/IVmKhGC5"><img src="email.svg"></img></a></div>
-      <div><a href="https://dribbble.com/adishasanic"><img src="dribbble.svg"></img></a></div>
-      <div><a href="https://www.linkedin.com/in/adis-hasanic/"><img src="linkedin.svg"></img></a></div>
-          
-        </div>
-        </div>
+      <div className="about">
+      <div className="about-width">
+      
+      <div className="intro">
+      <div className="portrait"><img src="adishasanic.jpg"></img></div>
+      <div className="text">I’m Adis, a 25-year-old UI/UX, web and visual designer and creative from Bosnia and Herzegovina. Past 10 years at early-stage startups creating and selling digital products that people need, want and can use. It’s all screen and storytelling.</div>
+      <div className="cta">
+      <Link href="mailto: adishasanic96@gmail.com"><a href="mailto:adishasanic96@gmail.com"><div className="button text-only">Email</div></a></Link>
+      <Link href="stacker-2021"><a href="stacker-2021"><div className="button linkedin"><img src="linkedin.svg"></img></div></a></Link>
       </div>
-      <div className="work">
-        <div>
-        <div className="font-300-bold title">Stacker</div>
-        <div className="font-100 description">Create business software without coding, based on existing data in connected data sources (Airtable, Google Sheets, etc.)</div>
-        <div className="arrows">
-        <Link href=""><div className="inactive"><img src="arrow-left-web.svg"></img></div></Link>
-          <Link href="flowdash"><a href="flowdash"><div><img src="arrow-right-web.svg"></img></div></a></Link>
-        </div>
-        </div>
       </div>
-
-      <div className="arrows-mobile">
-      <a><Link href=""><div className="inactive"><img src="arrow-left-mobile.svg"></img></div></Link></a>
-          <Link href="flowdash"><a href="flowdash"><div><img src="arrow-right-mobile.svg"></img></div></a></Link>
-        </div>
-
-      <div className="presentation twelve-eighty">
-      <div><img src="portfolio/product/stacker-2021/app-1.jpg"></img></div>
-      <div><img src="portfolio/product/stacker-2021/app-2.jpg"></img></div>
-          <div><img src="portfolio/product/stacker-2021/app-3.jpg"></img></div>
-      <div><img src="portfolio/product/stacker-2021/app-settings-1.jpg"></img></div>
-      <div><img src="portfolio/product/stacker-2021/app-settings-2.jpg"></img></div>
-      <div><img src="portfolio/product/stacker-2021/app-settings-3.jpg"></img></div>
-      <div><img src="portfolio/product/stacker-2021/app-settings-4.jpg"></img></div>
-      <div><img src="portfolio/product/stacker-2021/app-settings-5.jpg"></img></div>
-          <div><img src="portfolio/product/stacker-2021/app-settings-6.jpg"></img></div>
-              <div><img src="portfolio/product/stacker-2021/app-settings-7.jpg"></img></div>
-                  <div><img src="portfolio/product/stacker-2021/app-settings-8.jpg"></img></div>
-      <div><img src="portfolio/product/stacker-2021/profile-settings-1.jpg"></img></div>
+      
+      <div className="reference">
+      <div className="text">Working with Adis has completely transformed our product. His eye for design is incredible, and he’s managed to find elegant solutions to even the most knotty and difficult problems we’ve thrown at him. <span className="when">2021</span></div>
+      
+      <div className="who">
+      <div className="portrait"><img src="references/michael-skelly.jpg"></img></div>
+      <div className="info">
+      <div className="name">Michael Skelly <a href="https://uk.linkedin.com/in/skellymichael"><img src="linkedin-button.svg"></img></a></div>
+      <div className="role">Co-founder and CEO at Stacker</div>
+      <div className="description">Stacker is an early-stage startup making it easy to create apps without coding, with headquarters in London, backed by YCombinator, Andreessen Horowitz and Initialized Capital.</div>
+      </div>
+      </div>
+      
+      </div>
+      
+      <div className="reference">
+      <div className="text">Adis does top quality design work and is fast and very responsive to clients needs. I strongly recommend him without any reservations. <span className="when">2019</span></div>
+      
+      <div className="who">
+      <div className="portrait"><img src="references/chandra-duggirala.jpg"></img></div>
+      <div className="info">
+      <div className="name">Chandra Duggirala <a href="https://www.linkedin.com/in/csentropy"><img src="linkedin-button.svg"></img></a></div>
+      <div className="role">Entrepreneur</div>
+      <div className="description">Chandra is a serial entrepreneur based in San Francisco. Chandra started Fuel, backed by 500 Startups, and hired me to help him launch 3 products over the course of 3 years.</div>
+      </div>
+      </div>
+      
+      </div>
+      
+      
+      
+      <div className="reference">
+      <div className="text">From day one, Adis was able to massively contribute to the UX of our platform. He thoughtfully approaches his design decisions and takes the time to understand the individual problems of each user flow. He is always quick to respond and has created a visual design for our product that we are extremely happy with! <span className="when">2018</span></div>
+      
+      <div className="who">
+      <div className="portrait"><img src="references/blake-puryear.jpg"></img></div>
+      <div className="info">
+      <div className="name">Blake Puryear <a href="https://www.linkedin.com/in/bpuryear/"><img src="linkedin-button.svg"></img></a></div>
+      <div className="role">Product Lead at Engine eCommerce</div>
+      <div className="description">Blake hired me to design Engine's admin panel, to make it easy for anyone to set up and manage an online store using Engine. We worked closely via Slack for several months.</div>
+      </div>
+      </div>
+      
+      </div>
+      
+      
+      <div className="reference">
+      <div className="text">Adis took direction well, and was comfortable iterating through completion to create exactly what we were looking for. <span className="when">2017</span></div>
+      
+      <div className="who">
+      <div className="portrait"><img src="references/kelsey-klein.jpg"></img></div>
+      <div className="info">
+      <div className="name">Kelsey Klein <a href="https://www.linkedin.com/in/kelseyklein"><img src="linkedin-button.svg"></img></a></div>
+      <div className="role">Project Manager at Meitu</div>
+      <div className="description">Meitu was a startup at Series C providing photo-editing mobile apps. Kelsey was based in LA and hired me to design a set of infographics.</div>
+      </div>
+      </div>
+      
+      </div>
+      
+      
+      <div className="reference">
+      <div className="text">Adis is an excellent communicator, creates great designs fast, and is really great to work with. He quickly understood our rather complex requirements, did a bunch of quick iterations producing great work, was very flexible and patient as we came up with new requests, and also helped shape the overall product we were building. <span className="when">2016</span></div>
+      
+      <div className="who">
+      <div className="portrait"><img src="references/indraneel-mukherjee.jpg"></img></div>
+      <div className="info">
+      <div className="name">Indraneel Mukherjee <a href="https://www.linkedin.com/in/indraneelmukherjee"><img src="linkedin-button.svg"></img></a></div>
+      <div className="role">Co-founder and CEO at LiftIgniter</div>
+      <div className="description">LiftIgniter was a San Francisco based startup providing API for content recommendations, backed by YCombinator. I was hired to do freelance work on several occassions.</div>
+      </div>
+      </div>
+      
+      </div>
+      
+      <div className="reference">
+      <div className="text">Adis's skills in both communicating and actually displaying the message I want my audience to hear are just mind blowing. Adis's designs are definitely good looking and really present a nice user experience. To put it into context of dating: it's like dating a super-hot model, that is really smart. <span className="when">2015</span></div>
+      
+      <div className="who">
+      <div className="portrait"><img src="references/emil-hajric.jpg"></img></div>
+      <div className="info">
+      <div className="name">Emil Hajric <a href="https://www.linkedin.com/in/emilhajric"><img src="linkedin-button.svg"></img></a></div>
+      <div className="role">Founder and CEO at Helpjuice</div>
+      <div className="description">Helpjuice was a startup providing a web platform that makes it easy to build a knowledge base, backed by TechStars. I was hired to do freelance work on several occasions.</div>
+      </div>
+      </div>
+      
+      </div>
+      
+      
+      <div className="reference">
+      <div className="text">Adis is easy to work with and fast. After sharing who our target customers are, a sketch of our landing page, and our copy, he was able to make suggestions to our original thinking and create a beautiful design in just three days. <span className="when">2015</span></div>
+      
+      <div className="who">
+      <div className="portrait"><img src="references/tiffany-kuo.jpg"></img></div>
+      <div className="info">
+      <div className="name">Tiffany Kuo <a href="https://www.linkedin.com/in/tiffanyamykuo/"><img src="linkedin-button.svg"></img></a></div>
+      <div className="role">Co-founder and CEO at MultiplyLabs</div>
+      <div className="description">MultiplyLabs was a YC-backed San Francisco based startup providing a customizable pill. I was hired to design a coming-soon landing page that explains and announces the idea.</div>
+      </div>
+      </div>
+      
+      </div>
+      
+      
+      <div className="reference">
+      <div className="text">Adis’ opus at Kamcord was establishing the user interface, visual design and interactions for the Android Broadcasting app. He communicates his thought process well and continually strives to better understand his users. <span className="when">2014</span></div>
+      
+      <div className="who">
+      <div className="portrait"><img src="references/gordon-tindall.jpg"></img></div>
+      <div className="info">
+      <div className="name">Gordon Tindall <a href="https://www.linkedin.com/in/gordontindall"><img src="linkedin-button.svg"></img></a></div>
+      <div className="role">Senior Product Designer at Kamcord</div>
+      <div className="description">Kamcord was a 40-people startup at Series A with offices in US, China and Japan. I did contract work for 4 months on-site at Kamcord's San Francisco offices, managed by Gordon.</div>
+      </div>
+      </div>
+      
+      </div>
+      
+      <div className="reference">
+      <div className="text">Adis is skilled well beyond his years and has natural talent that's not easy to come by. He is professional, diligent in his process, and not afraid to push boundaries and innovate. The future is bright. <span className="when">2014</span></div>
+      
+      <div className="who">
+      <div className="portrait"><img src="references/nick-macario.jpg"></img></div>
+      <div className="info">
+      <div className="name">Nick Macario <a href="https://www.linkedin.com/in/nickmacario"><img src="linkedin-button.svg"></img></a></div>
+      <div className="role">Entrepreneur</div>
+      <div className="description">I worked with Nick for 2 years — 1 year remotely while I was going to high school, and 1 year on-site in San Francisco. We built a mobile website builder and a personal website builder.</div>
+      </div>
+      </div>
+      
+      </div>
+      
+      <div className="reference">
+      <div className="text">We set up and worked on an online writing platform for professionals. Adis came up with good ideas, maintained contact with our focus group and was timely with his deadlines. <span className="when">2014</span></div>
+      
+      <div className="who">
+      <div className="portrait"><img src="references/stan-janssen.jpg"></img></div>
+      <div className="info">
+      <div className="name">Stan Janssen <a href="https://nl.linkedin.com/in/janssenstan"><img src="linkedin-button.svg"></img></a></div>
+      <div className="role">Software Engineer</div>
+      <div className="description">I collaborated with Stan to build a writing app. I designed it, he built it, and we were trying to sell it to people who do professional work as writers.</div>
+      </div>
+      </div>
+      
+      </div>
+      
+      
+      <div className="reference">
+      <div className="text">Adis was always able to deliver something new and supremely beautiful. He was key in exploring new UIs and visual designs for our 10M+ users. A true pleasure to work with (and you get kickass designs too). <span className="when">2013</span></div>
+      
+      <div className="who">
+      <div className="portrait"><img src="references/dawson-whitfield.jpg"></img></div>
+      <div className="info">
+      <div className="name">Dawson Whitfield <a href="https://www.linkedin.com/in/dawsonwhitfield/"><img src="linkedin-button.svg"></img></a></div>
+      <div className="role">Product Designer at Weebly</div>
+      <div className="description">Weebly was a Series B startup backed by YCombinator and Sequoia. I was hired by Weebly to design for 1 year a set of website templates for their official offering to millions of users.</div>
+      </div>
+      </div>
+      
+      </div>
+    
+    <div className="reference">
+      <div className="text">I consider two aspects important when working on a website design - communication and precision. Adis excelled at both. He always responded in a timely manner, and the final work was more than I expected. <span className="when">2012</span></div>
+      
+      <div className="who">
+      <div className="portrait"><img src="references/sandro-dzneladze.jpg"></img></div>
+      <div className="info">
+      <div className="name">Sandro Dzneladze <a href="https://www.linkedin.com/in/sandro-dzneladze-0b3069152/"><img src="linkedin-button.svg"></img></a></div>
+      <div className="role">Entrepreneur</div>
+      <div className="description">Sandro hired me to design a website which was about exploring Georgia as a holiday destination, and a personal website for his photography work.</div>
+      </div>
+      </div>
+      
+      </div>
+    
+    
+    <div className="reference">
+      <div className="text">Adis produces nothing short of grade A work with great attention to detail and mindfulness of client requirements. <span className="when">2011</span></div>
+      
+      <div className="who">
+      <div className="portrait"><img src="references/vincent-chou.jpg"></img></div>
+      <div className="info">
+      <div className="name">Vincent Chou <a href="https://www.linkedin.com/in/vincentchou1/"><img src="linkedin-button.svg"></img></a></div>
+      <div className="role">Co-founder and CEO at Playmonks</div>
+      <div className="description">Vincent hired me to to join his startup on a part-time basis, to design a web platform for discovering video games based on what friends played. I was 15.</div>
+      </div>
+      </div>
+      
+      </div>
+      
+      
+      </div>
       </div>
     </div>
   )
---
+++ pages/stacker-2021.js
@@ -0,0 +1,58 @@
+import Head from 'next/head'
+import Link from 'next/link'
+
+export default function Home() {
+  return (
+    <div>
+      <Head><title>Adis Hasanic - UX, UI, Web and Visual Designer</title>
+        <script async src="https://www.googletagmanager.com/gtag/js?id=UA-121629290-2"></script>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'UA-121629290-2');
+  `,
+          }}
+        /></Head>
+      <div className="me">
+        <div>
+        <Link href="/"><div><img src="arrow-left-web.svg"></img></div></Link>
+          
+        </div>
+      </div>
+      <div className="work">
+        <div>
+        <div className="font-300-bold title">Stacker</div>
+        <div className="font-100 description">Create business software without coding, based on existing data in connected data sources (Airtable, Google Sheets, etc.)</div>
+        <div className="arrows">
+        <Link href=""><div className="inactive"><img src="arrow-left-web.svg"></img></div></Link>
+          <Link href="flowdash"><a href="flowdash"><div><img src="arrow-right-web.svg"></img></div></a></Link>
+        </div>
+        </div>
+      </div>
+
+      <div className="arrows-mobile">
+      <a><Link href=""><div className="inactive"><img src="arrow-left-mobile.svg"></img></div></Link></a>
+          <Link href="flowdash"><a href="flowdash"><div><img src="arrow-right-mobile.svg"></img></div></a></Link>
+        </div>
+
+      <div className="presentation twelve-eighty">
+      <div><img src="portfolio/product/stacker-2021/app-1.jpg"></img></div>
+      <div><img src="portfolio/product/stacker-2021/app-2.jpg"></img></div>
+          <div><img src="portfolio/product/stacker-2021/app-3.jpg"></img></div>
+      <div><img src="portfolio/product/stacker-2021/app-settings-1.jpg"></img></div>
+      <div><img src="portfolio/product/stacker-2021/app-settings-2.jpg"></img></div>
+      <div><img src="portfolio/product/stacker-2021/app-settings-3.jpg"></img></div>
+      <div><img src="portfolio/product/stacker-2021/app-settings-4.jpg"></img></div>
+      <div><img src="portfolio/product/stacker-2021/app-settings-5.jpg"></img></div>
+          <div><img src="portfolio/product/stacker-2021/app-settings-6.jpg"></img></div>
+              <div><img src="portfolio/product/stacker-2021/app-settings-7.jpg"></img></div>
+                  <div><img src="portfolio/product/stacker-2021/app-settings-8.jpg"></img></div>
+      <div><img src="portfolio/product/stacker-2021/profile-settings-1.jpg"></img></div>
+      </div>
+    </div>
+  )
+}
