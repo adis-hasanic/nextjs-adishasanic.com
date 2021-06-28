a7c6abfb6076e97fd61fe9a8fc157c55ae1110ab
adis-hasanic <adisdesigner96@gmail.com>
2021-06-28 22:16:19 +0300

    test

---
+++ pages/liftigniter.js
@@ -0,0 +1,346 @@
+import Head from 'next/head'
+import Link from 'next/link'
+
+export default function Home() {
+  return (
+    <div>
+      <Head><title>Adis Hasanic</title>
+        <script async src="https://www.googletagmanager.com/gtag/js?id=UA-121629290-2"></script>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'UA-121629290-2');
+  `,
+          }}
+        /></Head>
+      
+      <div className="content">
+      
+      <div className="width">
+      <div className="nav">
+      <div className="text-100">
+      <Link href="/"><a href="/" className="selected">Back</a></Link>
+      </div>
+      </div>
+      </div>
+      
+      
+      <div className="width">
+      <div className="text">
+      <div className="text-500">
+      LiftIgniter
+      </div>
+      </div>
+      </div>
+      
+      <div className="width">
+      <div className="text">
+      <div className="text-100">
+      During 2017 for 5 months total, on a freelance basis full-time remotely, I was a product designer at LiftIgniter, an early-stage startup backed by YCombinator with headquarters in San Francisco, providing an API for accurate content recommendations within web or mobile app. 
+      </div>
+      </div>
+      </div>
+      
+      <div className="width">
+      <div className="text">
+      <div className="text-400">
+      Process
+      </div>
+      </div>
+      </div>
+      
+      <div className="width">
+      <div className="text">
+      <div className="text-100">
+      I worked closely with the project lead, often one of the co-founders of LiftIgniter, who explained the goals and draw sketches of their ideas, after which I'd go into research and creating drafts of designs, which we'd then discuss via email or via call, and iterate to completion.
+      </div>
+      </div>
+      </div>
+      
+      <div className="width">
+      <div className="image">
+      <img className="nine-sixty" src="portfolio/product/liftigniter/process.jpg"></img>
+      </div>
+      </div>
+      
+      <div className="width">
+      <div className="image">
+      <img className="nine-sixty border" src="portfolio/product/liftigniter/process-2.jpg"></img>
+      </div>
+      </div>
+      
+      <div className="width">
+      <div className="text">
+      <div className="text-400">
+      Designs
+      </div>
+      </div>
+      </div>
+      
+      <div className="width">
+      <div className="text">
+      <div className="text-300">
+      Onboarding
+      </div>
+      </div>
+      </div>
+      
+      <div className="width">
+      <div className="text">
+      <div className="text-100">
+      I worked with Indraneel Mukherjee, CEO of LiftIgniter to design an onboarding process, which is the process of software engineer at a company which is LiftIgniter's customer, setting up LiftIgniter API at their app. Indraneel was a software engineer as well, so he had a clear idea what the necessary steps of the process are and what the user needs to understand.
+      </div>
+      </div>
+      </div>
+      
+      <div className="width">
+      <div className="text">
+      <div className="text-200">
+      First wireframes
+      </div>
+      </div>
+      </div>
+      
+      <div className="width">
+      <div className="text">
+      <div className="text-100">
+      Since there is a lot that goes into process, I created some wireframes to get a better understanding of what should constitute it, and to start going in some direction so that we can start discussing. 
+      </div>
+      </div>
+      </div>
+      
+      <div className="width">
+      <div className="image">
+      <img className="one-thousand border" src="portfolio/product/liftigniter/onboarding-draft-1/1.jpg"></img>
+      </div>
+      </div>
+      
+      <div className="width">
+      <div className="image">
+      <img className="one-thousand border" src="portfolio/product/liftigniter/onboarding-draft-1/2.jpg"></img>
+      </div>
+      </div>
+      
+      <div className="width">
+      <div className="image">
+      <img className="one-thousand border" src="portfolio/product/liftigniter/onboarding-draft-1/3.jpg"></img>
+      </div>
+      </div>
+      
+      <div className="width">
+      <div className="image">
+      <img className="one-thousand border" src="portfolio/product/liftigniter/onboarding-draft-1/4.jpg"></img>
+      </div>
+      </div>
+      
+      <div className="width">
+      <div className="text">
+      <div className="text-200">
+      Second wireframes
+      </div>
+      </div>
+      </div>
+      
+      <div className="width">
+      <div className="text">
+      <div className="text-100">
+      We agreed on the structure where steps are in sidebar, and from there it was mostly about going into details of what the steps should be and how to explain each step.
+      </div>
+      </div>
+      </div>
+      
+      <div className="width">
+      <div className="image">
+      <img className="one-thousand border" src="portfolio/product/liftigniter/onboarding-draft-2/1.jpg"></img>
+      </div>
+      </div>
+      
+      <div className="width">
+      <div className="image">
+      <img className="one-thousand border" src="portfolio/product/liftigniter/onboarding-draft-2/2.jpg"></img>
+      </div>
+      </div>
+      
+      <div className="width">
+      <div className="image">
+      <img className="one-thousand border" src="portfolio/product/liftigniter/onboarding-draft-2/3.jpg"></img>
+      </div>
+      </div>
+      
+      <div className="width">
+      <div className="image">
+      <img className="one-thousand border" src="portfolio/product/liftigniter/onboarding-draft-2/4.jpg"></img>
+      </div>
+      </div>
+      
+      
+      <div className="width">
+      <div className="text">
+      <div className="text-200">
+      Final design
+      </div>
+      </div>
+      </div>
+      
+      <div className="width">
+      <div className="text">
+      <div className="text-100">
+      Once I had a good understanding of the steps and content, I created high-fidelity mockups which we continued iterating for several weeks to make it simple in explaining the process visually and in words. After that, I created pixel-perfect prototype in HTML/CSS and sent it to developers.
+      </div>
+      </div>
+      </div>
+      
+      <div className="width">
+      <div className="image">
+      <img className="twelve-eighty" src="portfolio/product/liftigniter/onboarding-draft-3/4.jpg"></img>
+      </div>
+      </div>
+      
+      <div className="width">
+      <div className="image">
+      <img className="twelve-eighty" src="portfolio/product/liftigniter/onboarding-draft-3/5.jpg"></img>
+      </div>
+      </div>
+      
+      <div className="width">
+      <div className="image">
+      <img className="twelve-eighty" src="portfolio/product/liftigniter/onboarding-draft-3/7.jpg"></img>
+      </div>
+      </div>
+      
+      <div className="width">
+      <div className="image">
+      <img className="twelve-eighty" src="portfolio/product/liftigniter/onboarding-draft-3/8.jpg"></img>
+      </div>
+      </div>
+      
+      <div className="width">
+      <div className="image">
+      <img className="twelve-eighty" src="portfolio/product/liftigniter/onboarding-draft-3/9.jpg"></img>
+      </div>
+      </div>
+      
+      <div className="width">
+      <div className="image">
+      <img className="twelve-eighty" src="portfolio/product/liftigniter/onboarding-draft-3/10.jpg"></img>
+      </div>
+      </div>
+      
+      <div className="width">
+      <div className="image">
+      <img className="twelve-eighty" src="portfolio/product/liftigniter/onboarding-draft-3/11.jpg"></img>
+      </div>
+      </div>
+      
+      <div className="width">
+      <div className="image">
+      <img className="twelve-eighty" src="portfolio/product/liftigniter/onboarding-draft-3/12.jpg"></img>
+      </div>
+      </div>
+      
+      <div className="width">
+      <div className="image">
+      <img className="twelve-eighty" src="portfolio/product/liftigniter/onboarding-draft-3/13.jpg"></img>
+      </div>
+      </div>
+      
+      <div className="width">
+      <div className="image">
+      <img className="twelve-eighty" src="portfolio/product/liftigniter/onboarding-draft-3/14.jpg"></img>
+      </div>
+      </div>
+      
+      <div className="width">
+      <div className="text">
+      <div className="text-400">
+      Admin panel
+      </div>
+      </div>
+      </div>
+      
+      <div className="width">
+      <div className="text">
+      <div className="text-100">
+      I also worked on various parts of dashboard and admin panel, where users can configure API to better fit their needs, and track the results.
+      </div>
+      </div>
+      </div>
+      
+      <div className="width">
+      <div className="image">
+      <img className="twelve-eighty" src="portfolio/product/liftigniter/dashboard/1.jpg"></img>
+      </div>
+      </div>
+      
+      <div className="width">
+      <div className="image">
+      <img className="twelve-eighty" src="portfolio/product/liftigniter/dashboard/2.jpg"></img>
+      </div>
+      </div>
+      
+      <div className="width">
+      <div className="image">
+      <img className="twelve-eighty" src="portfolio/product/liftigniter/dashboard/3.jpg"></img>
+      </div>
+      </div>
+      
+      
+      <div className="width">
+      <div className="text">
+      <div className="text-400">
+      Effect
+      </div>
+      </div>
+      </div>
+      
+      <div className="width">
+      <div className="text">
+      <div className="text-100">
+      The project leads were software engineers themselves, so they had a good understanding of the users and the problems. They were very happy with the work and implemented all the designs into live product.
+      </div>
+      </div>
+      </div>
+      
+      
+      <div className="width">
+      <div className="text">
+      <div className="text-400">
+      Reach out anytime about anything.
+      </div>
+      </div>
+      </div>
+      
+      <div className="width">
+      <div className="text">
+      <div className="text-100">
+      Available for freelance work remotely for an early-stage startup (1-50 people), on an hourly, weekly, monthly, quarterly or project basis.
+      </div>
+      </div>
+      </div>
+      
+      
+      <div className="width">
+      <div className="text">
+      <form action="https://getform.io/f/240704a8-5047-49a9-9dcd-0180f07ccf03" method="POST">
+
+      
+      <div className="text-100">Your email address</div>
+  <input type="email" name="email" required></input>
+      
+      <div className="text-100">Message</div>
+  <textarea type="text" name="message" required></textarea>
+  <button type="submit">Send</button>
+
+</form>
+      </div>
+      </div>
+      
+      </div>
+      
+    </div>
+  )
+}
